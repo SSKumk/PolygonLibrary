799be0a14e9ae4bcd3e04b373b255d180517deff
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2023-03-01 20:46:43 +0500

    Правки ошибок в процедуре

--- PolygonLibrary/PolygonLibrary/Polygons/ConvexPolygons/Intersection.cs
+++ PolygonLibrary/PolygonLibrary/Polygons/ConvexPolygons/Intersection.cs
@@ -9,16 +9,11 @@ namespace PolygonLibrary.Polygons.ConvexPolygons;
 
 public partial class ConvexPolygon {
 
-  #region Convex polygon utilities
-
+#region Convex polygon utilities
   /// <summary>
   /// Flags for IntersectionPolygon procedure
   /// </summary>
-  private enum InsideType {
-    Unknown
-  , InP
-  , InQ
-  }
+  private enum InsideType { Unknown, InP, InQ }
 
   /// <summary>
   /// Smart adding a point to a list
@@ -31,9 +26,16 @@ public partial class ConvexPolygon {
     }
   }
 
+  /// <summary>
+  /// Moves to next vertex and if needed current vertex added to list 
+  /// </summary>
+  /// <param name="R">The list of vertices of polygon to be build</param>
+  /// <param name="vertex">The vertex to be added</param>
+  /// <param name="countVertex">Current counter</param>
+  /// <param name="needAdd">The flag determines will vertex be added or not</param>
   private static void Move(List<Point2D> R, Point2D vertex, ref int countVertex, bool needAdd) {
     if (needAdd) {
-      R.Add(vertex);
+      AddPoint(R, vertex);
     }
 
     countVertex++;
@@ -45,23 +47,21 @@ public partial class ConvexPolygon {
   /// <param name="P">The first convex polygon</param>
   /// <param name="Q">The second convex polygon</param>
   /// <returns>The resultant convex polgon</returns>
-  public ConvexPolygon IntersectionPolygon(ConvexPolygon P, ConvexPolygon Q) {
+  public static ConvexPolygon IntersectionPolygon(ConvexPolygon P, ConvexPolygon Q) {
     List<Point2D> R      = new List<Point2D>();
     InsideType    inside = InsideType.Unknown;
 
-    int     lenP   = P.Vertices.Count;
-    int     lenQ   = Q.Vertices.Count;
-    int     countP = 1;
-    int     countQ = 1;
-    Point2D p      = P.Vertices[0];
-    Point2D q      = Q.Vertices[0];
-    Point2D pred_p;
-    Point2D pred_q;
+    int lenP   = P.Vertices.Count;
+    int lenQ   = Q.Vertices.Count;
+    int countP = 1;
+    int countQ = 1;
+    // int advP   = 0; //Will increase only after first intersection
+    // int advQ   = 0;
     do {
-      pred_p = p;
-      pred_q = q;
-      p      = P.Vertices.GetAtCyclic(countP);
-      q      = Q.Vertices.GetAtCyclic(countQ);
+      Point2D pred_p = P.Vertices.GetAtCyclic(countP - 1);
+      Point2D pred_q = Q.Vertices.GetAtCyclic(countQ - 1);
+      Point2D p      = P.Vertices.GetAtCyclic(countP);
+      Point2D q      = Q.Vertices.GetAtCyclic(countQ);
 
       Segment   hat_p     = P.Contour.Edges.GetAtCyclic(countP - 1);
       Segment   hat_q     = Q.Contour.Edges.GetAtCyclic(countQ - 1);
@@ -69,10 +69,14 @@ public partial class ConvexPolygon {
       Vector2D  hat_p0    = hat_p.directional.Normalize();
       Vector2D  hat_q0    = hat_q.directional.Normalize();
 
-      int qInHp = Tools.CMP((q - pred_p).Normalize() ^ hat_p0);
-      int pInHq = Tools.CMP((p - pred_q).Normalize() ^ hat_q0);
+      Vector2D aux_q = q - pred_p;
+      Vector2D aux_p = p - pred_q;
+      aux_q = aux_q.CompareTo(new Vector2D(0, 0)) == 0 ? aux_q : aux_q.Normalize();
+      aux_p = aux_p.CompareTo(new Vector2D(0, 0)) == 0 ? aux_p : aux_p.Normalize();
+      int qInHp = Tools.CMP(hat_p0 ^ aux_q);
+      int pInHq = Tools.CMP(hat_q0 ^ aux_p);
       if (crossInfo.crossType == CrossType.SinglePoint) {
-        if (R.Count != 0) {
+        if (R.Count > 2) {
           if (crossInfo.p.Equals(R.First())) {
             return new ConvexPolygon(R);
           }
@@ -80,13 +84,12 @@ public partial class ConvexPolygon {
 
         if (pInHq > 0) {
           inside = InsideType.InP;
-        }
-        else if (qInHp > 0) {
+        } else if (qInHp > 0) {
           inside = InsideType.InQ;
         }
 
         //Else keep 'inside'
-        AddPoint(R, p);
+        AddPoint(R, crossInfo.p);
       }
 
       int cross = Tools.CMP(hat_p.directional ^ hat_q.directional);
@@ -95,32 +98,28 @@ public partial class ConvexPolygon {
           return null; //todo И страдать правильным образом
         case CrossType.Overlap:
           Move(R, crossInfo.p, ref countP, false);
-
           break;
         default: {
           if (Tools.GE(cross, 0)) {
             if (qInHp > 0) {
-              Move(R, crossInfo.p, ref countP, inside == InsideType.InP);
+              Move(R, p, ref countP, inside == InsideType.InP);
+            } else {
+              Move(R, q, ref countQ, inside == InsideType.InQ);
             }
-            else {
-              Move(R, crossInfo.p, ref countQ, inside == InsideType.InQ);
-            }
-          }
-          else {
+          } else {
             if (pInHq > 0) {
-              Move(R, crossInfo.p, ref countQ, inside == InsideType.InQ);
-            }
-            else {
-              Move(R, crossInfo.p, ref countP, inside == InsideType.InP);
+              Move(R, q, ref countQ, inside == InsideType.InQ);
+            } else {
+              Move(R, p, ref countP, inside == InsideType.InP);
             }
           }
-
           break;
         }
       }
-    } while ((countP < lenP || countQ < lenQ) && countP < 2 * lenP && countQ < 2 * lenQ);
+    } while (countP + countQ < 2 * (lenP + lenQ));
+    // } while ((countP < lenP || countQ < lenQ) && countP < 2 * lenP && countQ < 2 * lenQ); todo Потенциально можно быстрее 
 
-    if (R.Count != 0) {
+    if (R.Count > 2) {
       return new ConvexPolygon(R);
     }
 
@@ -134,7 +133,6 @@ public partial class ConvexPolygon {
 
     return null;
   }
-
-  #endregion
+#endregion
 
 }
