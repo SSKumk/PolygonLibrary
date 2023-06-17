0f8a0d77d9b3bfd452e17d9b55091610fb7b51d7
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2023-06-17 15:44:57 +0500

    Конструктор принимает ещё вершины и информацию о смежности

--- PolygonLibrary/PolygonLibrary/Polyhedra/ConvexPolyhedra/NonSimplex.cs
+++ PolygonLibrary/PolygonLibrary/Polyhedra/ConvexPolyhedra/NonSimplex.cs
@@ -31,18 +31,37 @@ public class NonSimplex : BaseConvexPolyhedron {
   /// Construct a new instance of the <see cref="NonSimplex"/> class based on it's faces. 
   /// </summary>
   /// <param name="faces">Faces to construct the convex polyhedron</param>
-  public NonSimplex(HashSet<BaseConvexPolyhedron> faces) {
+  /// <param name="Vs">The vertices of polyhedron. </param>
+  /// <param name="incid">The information about neighbours-faces of edges.</param>
+  /// <param name="fansInfo">The information about faces incident to this point.</param>
+  public NonSimplex(HashSet<BaseConvexPolyhedron> faces
+                  , HashSet<Point>?               Vs       = null
+                  , IncidenceInfo?                incid    = null
+                  , FansInfo?                     fansInfo = null) {
     Dim   = faces.First().Dim + 1;
     Type  = ConvexPolyhedronType.NonSimplex;
     Faces = faces;
 
-    HashSet<Point> tempVertices = new HashSet<Point>();
+    if (Vs is not null) {
+      Vertices = new HashSet<Point>(Vs);
+    } else {
+      HashSet<Point> tempVertices = new HashSet<Point>();
 
-    foreach (BaseConvexPolyhedron face in faces) {
-      tempVertices.UnionWith(face.Vertices);
+      foreach (BaseConvexPolyhedron face in faces) {
+        tempVertices.UnionWith(face.Vertices);
+      }
+
+      Vertices = tempVertices;
     }
 
-    Vertices = tempVertices;
+
+    if (incid is not null) {
+      _faceIncidence = new IncidenceInfo(incid);
+    }
+
+    if (fansInfo is not null) {
+      _fans = new FansInfo(fansInfo);
+    }
   }
 
 }
