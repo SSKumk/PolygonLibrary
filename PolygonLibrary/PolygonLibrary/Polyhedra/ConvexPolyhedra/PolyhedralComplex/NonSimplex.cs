07bafa867a474d4793c2c126bd32e800a25b0335
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2023-06-20 11:34:04 +0500

    PComplex ждёт лучших времён

---
+++ PolygonLibrary/PolygonLibrary/Polyhedra/ConvexPolyhedra/PolyhedralComplex/NonSimplex.cs
@@ -0,0 +1,67 @@
+// using System.Collections.Generic;
+// using System.Linq;
+// using PolygonLibrary.Basics;
+//
+// namespace PolygonLibrary.Polyhedra.ConvexPolyhedra.PolyhedralComplex;
+//
+// public class NonSimplex : BaseConvexPolyhedron {
+//
+//   /// <summary>
+//   /// Gets the dimension of the polyhedron.
+//   /// </summary>
+//   public override int Dim { get; }
+//
+//
+//   /// <summary>
+//   /// Gets the type of the convex polyhedron.
+//   /// </summary>
+//   public override ConvexPolyhedronType Type { get; }
+//
+//   /// <summary>
+//   /// Gets the set of vertices of the polyhedron.
+//   /// </summary>
+//   public override HashSet<Point> Vertices { get; }
+//
+//   /// <summary>
+//   /// Gets the set of (d-1)-dimensional faces of the polyhedron.
+//   /// </summary>
+//   public override HashSet<BaseConvexPolyhedron> Faces { get; }
+//
+//   /// <summary>
+//   /// Construct a new instance of the <see cref="NonSimplex"/> class based on it's faces. 
+//   /// </summary>
+//   /// <param name="faces">Faces to construct the convex polyhedron</param>
+//   /// <param name="Vs">The vertices of polyhedron. </param>
+//   /// <param name="incid">The information about neighbours-faces of edges.</param>
+//   /// <param name="fansInfo">The information about faces incident to this point.</param>
+//   public NonSimplex(HashSet<BaseConvexPolyhedron> faces
+//                   , HashSet<Point>?               Vs       = null
+//                   , SubIncidenceInfo?                incid    = null
+//                   , FansInfo?                     fansInfo = null) {
+//     Dim   = faces.First().Dim + 1;
+//     Type  = ConvexPolyhedronType.NonSimplex;
+//     Faces = faces;
+//
+//     if (Vs is not null) {
+//       Vertices = new HashSet<Point>(Vs);
+//     } else {
+//       HashSet<Point> tempVertices = new HashSet<Point>();
+//
+//       foreach (BaseConvexPolyhedron face in faces) {
+//         tempVertices.UnionWith(face.Vertices);
+//       }
+//
+//       Vertices = tempVertices;
+//     }
+//
+//
+//     if (incid is not null) {
+//       _faceIncidence = new SubIncidenceInfo(incid);
+//     }
+//
+//     if (fansInfo is not null) {
+//       _fans = new FansInfo(fansInfo);
+//     }
+//   }
+//
+// }
