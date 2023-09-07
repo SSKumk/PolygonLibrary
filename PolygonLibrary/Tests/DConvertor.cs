2270a84cf955fa82762f29d44c84d2b0d798b5a6
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2023-09-07 14:23:17 +0500

    Create DConvertor.cs

---
+++ PolygonLibrary/Tests/DConvertor.cs
@@ -0,0 +1,12 @@
+using CGLibrary;
+
+public class DConvertor : INumConvertor<double> {
+
+  public static double ToDouble(double   from) => from;
+  public static double FromDouble(double from) => from;
+  public static int    ToInt(double      from) => (int) from;
+  public static double FromInt(int       from) => from;
+  public static uint   ToUInt(double     from) => (uint)from;
+  public static double FromUInt(uint     from) => from;
+
+}
