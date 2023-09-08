42d3974f411e74e94e3e9addd13e2484696b7c61
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2023-09-08 15:56:58 +0500

    Update OtherTests.cs

--- PolygonLibrary/Tests/OtherTests/OtherTests.cs
+++ PolygonLibrary/Tests/OtherTests/OtherTests.cs
@@ -1,19 +1,27 @@
 using NUnit.Framework;
 using DoubleDouble;
+
 // using DG = CGLibrary.Geometry<DoubleDouble.ddouble, Convertors.DDConvertor>;
 // using G = CGLibrary.Geometry<double, Convertors.DConvertor>;
 namespace OtherTests;
 
 [TestFixture]
-public class Atan2Test {
+public class OtherTests {
 
   [Test]
-  public void Atan2DoubleTest() {
+  public void Atan2Test() {
     var x = ddouble.Atan2(-1e-15, -1);
     var y = double.Atan2(-1e-15, -1);
     Console.WriteLine(x);
     Console.WriteLine(y);
   }
 
+  [Test]
+  public void AcosTest() {
+    var x = ddouble.Acos(1);
+    var y = double.Acos(1);
+    Console.WriteLine(x);
+    Console.WriteLine(y);
+  }
 
 }
