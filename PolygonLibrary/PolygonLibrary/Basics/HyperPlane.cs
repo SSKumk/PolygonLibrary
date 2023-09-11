37a6ee15495c3903dc92c99a687b760bc745dae4
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2023-09-11 17:40:40 +0500

    Форматирование

--- PolygonLibrary/PolygonLibrary/Basics/HyperPlane.cs
+++ PolygonLibrary/PolygonLibrary/Basics/HyperPlane.cs
@@ -5,7 +5,8 @@ using System.Numerics;
 
 namespace CGLibrary;
 
-public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, ITrigonometricFunctions<TNum>, IPowerFunctions<TNum>, IRootFunctions<TNum>,
+public partial class Geometry<TNum, TConv>
+  where TNum : struct, INumber<TNum>, ITrigonometricFunctions<TNum>, IPowerFunctions<TNum>, IRootFunctions<TNum>,
   IFloatingPoint<TNum>
   where TConv : INumConvertor<TNum> {
 
@@ -13,22 +14,22 @@ public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, I
   /// Represents a (d-1)-dimensional hyperplane in a d-dimensional euclidean space.
   /// </summary>
   public class HyperPlane {
-  
+
     /// <summary>
     /// The point through which the hyperplane passes.
     /// </summary>
     public readonly Point Origin;
-  
+
     /// <summary>
     /// The dimension of the hyperplane.
     /// </summary>
     private readonly int _dim;
-  
+
     /// <summary>
     /// The dimension of the hyperplane.
     /// </summary>
     public int Dim => _dim;
-  
+
     /// <summary>
     /// The affine basis associated with the hyperplane.
     /// </summary>
@@ -39,21 +40,21 @@ public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, I
             int         spaceDim = Origin.Dim;
             LinearBasis lBasis   = new LinearBasis();
             lBasis.AddVector(Normal);
-  
+
             for (int i = 1; i <= spaceDim; i++) {
               Vector orth = Vector.CreateOrth(spaceDim, i);
               lBasis.AddVector(orth);
             }
-  
+
             _affineBasis = new AffineBasis(Origin, lBasis.Basis.GetRange(1, lBasis.BasisDim - 1));
           }
-  
+
           return _affineBasis;
         }
     }
-  
+
     private AffineBasis? _affineBasis = null;
-  
+
     /// <summary>
     /// The normal vector to the hyperplane.
     /// </summary>
@@ -63,28 +64,36 @@ public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, I
           if (_normal is null) {
             Debug.Assert(_affineBasis != null, nameof(_affineBasis) + " != null");
             int spaceDim = Origin.Dim;
-  
-  
+
+
             for (int i = 1; i <= spaceDim; i++) {
               Vector orth = Vector.CreateOrth(spaceDim, i);
               Vector n    = Vector.OrthonormalizeAgainstBasis(orth, _affineBasis.Basis);
-  
+
               if (!n.IsZero) {
                 _normal = n;
-  
+
                 break;
               }
             }
-            Debug.Assert(_normal is not null, "HyperPlane: Computation of the normal on the basis of the affine basis of the plane gives null vector reference!");
-            Debug.Assert(!_normal.IsZero, "HyperPlane: Computation of the normal on the basis of the affine basis of the plane gives zero vector!");
+            Debug.Assert
+              (
+               _normal is not null
+             , "HyperPlane: Computation of the normal on the basis of the affine basis of the plane gives null vector reference!"
+              );
+            Debug.Assert
+              (
+               !_normal.IsZero
+             , "HyperPlane: Computation of the normal on the basis of the affine basis of the plane gives zero vector!"
+              );
           }
-  
+
           return _normal!;
         }
     }
-  
+
     private Vector? _normal = null;
-  
+
     /// <summary>
     /// The constant term in the equation of the hyperplane.
     /// </summary>
@@ -94,13 +103,13 @@ public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, I
           if (_constantTerm is null) {
             _constantTerm = -(Normal * new Vector(Origin));
           }
-  
+
           return _constantTerm.Value;
         }
     }
-  
+
     private TNum? _constantTerm = null;
-  
+
     /// <summary>
     /// Constructs a hyperplane from a given point and normal vector.
     /// </summary>
@@ -111,7 +120,7 @@ public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, I
       _normal = normal;
       _dim    = Origin.Dim - 1;
     }
-  
+
     /// <summary>
     /// Constructs a hyperplane from a given affine basis.
     /// </summary>
@@ -126,16 +135,16 @@ public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, I
          affineBasis.SpaceDim == affineBasis.Origin.Dim - 1
        , $"Hyperplane should has (d-1) = {affineBasis.Origin.Dim - 1} independent vectors in its basis. Found {affineBasis.SpaceDim}"
         );
-  
+
       Origin       = affineBasis.Origin;
       _affineBasis = affineBasis;
       _dim         = Origin.Dim - 1;
-  
+
       if (toOrient is not null) {
         OrientNormal(toOrient.Value.point, toOrient.Value.isPositive);
       }
     }
-  
+
     /// <summary>
     /// Method to orient normal of the plane.
     /// </summary>
@@ -143,29 +152,29 @@ public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, I
     /// <param name="isPositive">If the bool is <c>true</c> than point is in positive semi-space, in the negative semi-space otherwise.</param>
     public void OrientNormal(Point point, bool isPositive) {
       TNum res = Eval(point);
-  
+
       Debug.Assert(Tools.NE(res), "For hyperplane orientation, a point is given, which belongs to the hyperplane");
-  
+
       if ((Tools.LT(res) && isPositive) || (Tools.GT(res) && !isPositive)) {
         _normal       = -_normal!;
         _constantTerm = -_constantTerm!;
       }
     }
-  
+
     /// <summary>
     /// Evaluates the equation of the hyperplane at a given point.
     /// </summary>
     /// <param name="point">The point at which the equation is to be evaluated.</param>
     /// <returns>The value of the equation of the hyperplane at the given point.</returns>
     public TNum Eval(Point point) { return Normal * new Vector(point) + ConstantTerm; }
-  
+
     /// <summary>
     /// Checks if the hyperplane contains a given point.
     /// </summary>
     /// <param name="point">The point to check.</param>
     /// <returns>True if the hyperplane contains the given point, otherwise False.</returns>
     public bool Contains(Point point) { return Tools.EQ(Eval(point)); }
-  
+
     /// <summary>
     /// Filters a given collection of points, leaving only those that lie on the hyperplane.
     /// </summary>
@@ -178,7 +187,7 @@ public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, I
         }
       }
     }
-  
+
     /// <summary>
     /// Filters a given collection of points, leaving only those that not lie on the hyperplane.
     /// </summary>
@@ -191,7 +200,7 @@ public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, I
         }
       }
     }
-  
+
     /// <summary>
     /// Determines if all the points in the given collection lie on the same side of the hyperplane.
     /// </summary>
@@ -203,24 +212,23 @@ public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, I
     /// -1 if all points lie in the opposite direction of the normal vector
     /// int.MaxValue if bool equals to False</returns>
     /// <remarks>If there are no points in the swarm then (true, int.MaxValue) returns.</remarks>
-    public (bool, int) AllAtOneSide(IEnumerable<Point> Swarm) {
+    public (bool atOneSide, int where) AllAtOneSide(IEnumerable<Point> Swarm) {
       if (!Swarm.Any()) {
         return (true, int.MaxValue);
       }
-  
+
       IEnumerable<int> temp = Swarm.Select(p => Tools.CMP(Eval(p))).Where(k => k != 0);
-  
+
       if (!temp.Any()) {
         return (true, 0);
       }
-  
+
       int  sign        = temp.First();
       bool isAtOneSide = temp.All(k => k == sign);
-  
+
       return (isAtOneSide, sign);
     }
-  
+
   }
 
 }
-
