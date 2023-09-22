c03f9e0d4cfb92b0ebf172d464ad103c210a4575
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2023-09-22 15:42:28 +0500

    Update OtherTests.cs

--- PolygonLibrary/Tests/OtherTests/OtherTests.cs
+++ PolygonLibrary/Tests/OtherTests/OtherTests.cs
@@ -1,12 +1,33 @@
-using System.Formats.Asn1;
 using System.Numerics;
 using CGLibrary;
-using NUnit.Framework;
+using Convertors;
 using DoubleDouble;
-using DG = CGLibrary.Geometry<DoubleDouble.ddouble, Convertors.DDConvertor>;
-using G = CGLibrary.Geometry<double, Convertors.DConvertor>;
+using NUnit.Framework;
+using ddG = Tests.OtherTests.AuxFunc<DoubleDouble.ddouble, Convertors.DDConvertor>;
+using dG = Tests.OtherTests.AuxFunc<double, Convertors.DConvertor>;
 
-namespace OtherTests;
+
+namespace Tests.OtherTests;
+
+public class AuxFunc<TNum, TConv> : ToolsTests.ToolsTests<TNum, TConv>
+  where TNum : struct, INumber<TNum>, ITrigonometricFunctions<TNum>, IPowerFunctions<TNum>, IRootFunctions<TNum>,
+  IFloatingPoint<TNum>
+  where TConv : INumConvertor<TNum> { }
+// /// <summary>
+// /// Rotates the given swarm of points in the space by given unitary matrix.
+// /// </summary>
+// /// <param name="S">The swarm of points to rotate.</param>
+// /// <param name="rotation">Matrix to rotate a swarm.</param>
+// /// <returns>The rotated swarm of points.</returns>
+// private static List<Geometry<TNum, TConv>.Point>
+//   Rotate<TNum, TConv>(IEnumerable<Geometry<TNum, TConv>.Point> S, Geometry<TNum, TConv>.Matrix rotation)
+//   where TNum : struct, INumber<TNum>, ITrigonometricFunctions<TNum>, IPowerFunctions<TNum>, IRootFunctions<TNum>,
+//   IFloatingPoint<TNum>
+//   where TConv : INumConvertor<TNum> {
+//   IEnumerable<Geometry<TNum, TConv>.Vector> rotated = S.Select(s => new Geometry<TNum, TConv>.Vector(s) * rotation);
+//
+//   return rotated.Select(v => new Geometry<TNum, TConv>.Point(v)).ToList();
+// }
 
 [TestFixture]
 public class OtherTests {
@@ -124,11 +145,12 @@ public class OtherTests {
 
         // Если размерность грани, куда нужно поместить точку меньше размерности куба
         foreach (List<int> fixedInd in allPoints) {
-          List<Geometry<TNum, TConv>.Point> smallCube = OtherTests.Cube(cubeDim - dim, out List<Geometry<TNum, TConv>.Point> _, 0);
+          List<Geometry<TNum, TConv>.Point> smallCube = OtherTests.Cube
+            (cubeDim - dim, out List<Geometry<TNum, TConv>.Point> _, 0);
           foreach (Geometry<TNum, TConv>.Point pointCube in smallCube) {
             for (int k = 0; k < amount; k++) {
               TNum[] point = new TNum[cubeDim];
-              int      s     = 0;
+              int    s     = 0;
               for (int j = 0; j < cubeDim; j++) {
                 point[j] = -TNum.One;
               }
@@ -140,7 +162,7 @@ public class OtherTests {
 
               for (int j = 0; j < cubeDim; j++) {
                 if (Geometry<TNum, TConv>.Tools.EQ(point[j], -TNum.One)) {
-                  point[j] = GenInner<TNum,TConv>(random);
+                  point[j] = GenInner<TNum, TConv>(random);
                 }
               }
 
@@ -154,24 +176,24 @@ public class OtherTests {
     return Cube;
   }
 
-  [Test]
-  public void Atan2Test() {
-    var x = ddouble.Atan2(-1e-15, -1);
-    var y = double.Atan2(-1e-15, -1);
+  // [Test]
+  // public void Atan2Test() {
+  //   var x = ddouble.Atan2(-1e-15, -1);
+  //   var y = double.Atan2(-1e-15, -1);
+  //
+  //   Console.WriteLine(x);
+  //   Console.WriteLine(y);
+  // }
+  //
+  // [Test]
+  // public void AcosTest() {
+  //   var x = ddouble.Acos(1);
+  //   var y = double.Acos(1);
+  //   Console.WriteLine(x);
+  //   Console.WriteLine(y);
+  // }
 
-    Console.WriteLine(x);
-    Console.WriteLine(y);
-  }
-
-  [Test]
-  public void AcosTest() {
-    var x = ddouble.Acos(1);
-    var y = double.Acos(1);
-    Console.WriteLine(x);
-    Console.WriteLine(y);
-  }
-
-  private List<G.Point> ToDPoints(List<DG.Point> from) {
+  private List<dG.Point> ToDPoints(List<ddG.Point> from) {
     return from.Select
                 (
                  p => {
@@ -180,22 +202,77 @@ public class OtherTests {
                      pDD[i] = (double)p[i];
                    }
 
-                   return new G.Point(pDD);
+                   return new dG.Point(pDD);
                  }
                 )
                .ToList();
   }
 
+
   [Test]
-  public void FindEtalon() {
-    List<DG.Point> cube4D_double_double = Cube(4, out List<DG.Point> pureCube_double, 0, new[] { 1, 2, 3, 4 }, 1);
-    List<G.Point> cube4D_double = ToDPoints(cube4D_double_double);
+  public void FindEtalon_3D() {
+    int        cubeDim = 3;
+    ddG.Matrix x1x2    = ddG.GenRotationMatrix(cubeDim, 1, 2, ddG.Tools.PI / 4);
+    ddG.Matrix x1x3    = ddG.GenRotationMatrix(cubeDim, 1, 3, ddG.Tools.PI / 4);
 
-    var P1 = G.GiftWrapping.WrapPolytop(cube4D_double);
-    var P2 = DG.GiftWrapping.WrapPolytop(cube4D_double_double);
+    ddG.Matrix x1x2x1x3 = x1x2 * x1x3;
 
-    Console.WriteLine($"{P1.Faces.First().Normal + G.Vector.CreateOrth(4, 4)}");
-    Console.WriteLine(P2.Faces.First().Normal + DG.Vector.CreateOrth(4,4));
+    List<ddG.Point> cube_DD             = Cube(cubeDim, out List<ddG.Point> _, 0, new[] { 1, 2, 3, 4 }, 5);
+    var             cube_DD_rotated     = ddG.Rotate(cube_DD, x1x2x1x3);
+    List<dG.Point>  cube_double_rotated = ToDPoints(cube_DD_rotated);
+
+
+    var P1 = dG.GiftWrapping.WrapPolytop(cube_double_rotated);
+    var P2 = ddG.GiftWrapping.WrapPolytop(cube_DD_rotated);
+
+    Console.WriteLine($"{P1.Faces.First().Normal}");
+    Console.WriteLine(P2.Faces.First().Normal);
+  }
+
+  [Test]
+  public void FindEtalon_4D() {
+    int        cubeDim = 4;
+    ddG.Matrix x1x2    = ddG.GenRotationMatrix(cubeDim, 1, 2, ddG.Tools.PI / 4);
+    ddG.Matrix x1x3    = ddG.GenRotationMatrix(cubeDim, 1, 3, ddG.Tools.PI / 4);
+    ddG.Matrix x1x4    = ddG.GenRotationMatrix(cubeDim, 1, 4, ddG.Tools.PI / 4);
+
+    ddG.Matrix x1x2x1x3x1x4 = x1x2 * x1x3 * x1x4;
+
+    List<ddG.Point> cube_DD             = Cube(cubeDim, out List<ddG.Point> _, 0, new[] { 1, 2, 3, 4 }, 5);
+    var             cube_DD_rotated     = ddG.Rotate(cube_DD, x1x2x1x3x1x4);
+    List<dG.Point>  cube_double_rotated = ToDPoints(cube_DD_rotated);
+
+
+    var P1 = dG.GiftWrapping.WrapPolytop(cube_double_rotated);
+    var P2 = ddG.GiftWrapping.WrapPolytop(cube_DD_rotated);
+
+    Console.WriteLine($"{P1.Faces.First().Normal}");
+    Console.WriteLine(P2.Faces.First().Normal);
+  }
+
+  [Test]
+  public void FindEtalon_8D() {
+    int        cubeDim = 8;
+    ddG.Matrix x1x2    = ddG.GenRotationMatrix(cubeDim, 1, 2, ddG.Tools.PI / 4);
+    ddG.Matrix x1x3    = ddG.GenRotationMatrix(cubeDim, 1, 3, ddG.Tools.PI / 4);
+    ddG.Matrix x1x4    = ddG.GenRotationMatrix(cubeDim, 1, 4, ddG.Tools.PI / 4);
+    ddG.Matrix x1x5    = ddG.GenRotationMatrix(cubeDim, 1, 5, ddG.Tools.PI / 4);
+    ddG.Matrix x1x6    = ddG.GenRotationMatrix(cubeDim, 1, 6, ddG.Tools.PI / 4);
+    ddG.Matrix x1x7    = ddG.GenRotationMatrix(cubeDim, 1, 7, ddG.Tools.PI / 4);
+    ddG.Matrix x1x8    = ddG.GenRotationMatrix(cubeDim, 1, 8, ddG.Tools.PI / 4);
+
+    ddG.Matrix x1_x8 = x1x2 * x1x3 * x1x4 * x1x5 * x1x6 * x1x7 * x1x8;
+
+    List<ddG.Point> cube_DD             = Cube(cubeDim, out List<ddG.Point> _, 0, new[] { 1, 2, 3, 4 }, 0);
+    var             cube_DD_rotated     = ddG.Rotate(cube_DD, x1_x8);
+    List<dG.Point>  cube_double_rotated = ToDPoints(cube_DD_rotated);
+
+
+    var P1 = dG.GiftWrapping.WrapPolytop(cube_double_rotated);
+    var P2 = ddG.GiftWrapping.WrapPolytop(cube_DD_rotated);
+
+    Console.WriteLine($"{P1.Faces.First().Normal}");
+    Console.WriteLine(P2.Faces.First().Normal);
   }
 
 }
