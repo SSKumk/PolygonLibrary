06650704e592249341cd98d74de975bb0740fd00
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2023-10-19 19:20:26 +0500

    Update SubTwoDimensionalEdge.cs

--- PolygonLibrary/PolygonLibrary/Polyhedra/ConvexPolyhedra/GiftWrapping/SubTwoDimensionalEdge.cs
+++ PolygonLibrary/PolygonLibrary/Polyhedra/ConvexPolyhedra/GiftWrapping/SubTwoDimensionalEdge.cs
@@ -1,16 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 
 namespace CGLibrary;
 
-public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, ITrigonometricFunctions<TNum>, IPowerFunctions<TNum>, IRootFunctions<TNum>,
+public partial class Geometry<TNum, TConv>
+  where TNum : struct, INumber<TNum>, ITrigonometricFunctions<TNum>, IPowerFunctions<TNum>, IRootFunctions<TNum>,
   IFloatingPoint<TNum>, IFormattable
   where TConv : INumConvertor<TNum> {
 
   /// <summary>
-  /// Represents a one-dimensional edge of a convex polytope expressed in 2D-coordinates.
+  /// Represents a one-dimensional edge of a convex polytope expressed in k-space.
   /// </summary>
   public class SubTwoDimensionalEdge : BaseSubCP {
 
@@ -25,17 +27,9 @@ public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, I
     public override SubCPType Type => SubCPType.OneDimensional;
 
     /// <summary>
-    /// Gets the 2D-vertices of the edge.
+    /// The faces of 1-dimensional edge is its vertex.
     /// </summary>
-    public override HashSet<SubPoint> Vertices { get; }
-
-    private SubPoint first  { get; }
-    private SubPoint second { get; }
-
-    /// <summary>
-    /// There are no Faces of the 1-dimensional edge.
-    /// </summary>
-    public override HashSet<BaseSubCP>? Faces => null;
+    public override HashSet<BaseSubCP> Faces { get; }
 
     /// <summary>
     /// There are no Faces of the 1-dimensional edge.
@@ -48,28 +42,40 @@ public partial class Geometry<TNum, TConv> where TNum : struct, INumber<TNum>, I
     /// <param name="first">The first vertex of the edge.</param>
     /// <param name="second">The second vertex of the edge.</param>
     public SubTwoDimensionalEdge(SubPoint first, SubPoint second) {
-      this.first  = first;
-      this.second = second;
-      HashSet<SubPoint> vertices = new HashSet<SubPoint>() { first, second };
-      Vertices = vertices;
+      Faces = new HashSet<BaseSubCP>() { new SubZeroDimensional(first), new SubZeroDimensional(second) };
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SubTwoDimensionalEdge"/> class base on ZeroDimensional.
+    /// </summary>
+    /// <param name="first">The first vertex of the edge.</param>
+    /// <param name="second">The second vertex of the edge.</param>
+    public SubTwoDimensionalEdge(SubZeroDimensional first, SubZeroDimensional second) {
+      Faces = new HashSet<BaseSubCP>() { first, second };
     }
 
     /// <summary>
     /// Converts the edge to the previous space.
     /// </summary>
     /// <returns>The converted edge in the previous space.</returns>
-    public override BaseSubCP ToPreviousSpace() => new SubTwoDimensionalEdge(first.Parent!, second.Parent!);
+    public override BaseSubCP ToPreviousSpace() => new SubTwoDimensionalEdge
+      ((SubZeroDimensional)Faces.First().ToPreviousSpace(), (SubZeroDimensional)Faces.Last().ToPreviousSpace());
 
     /// <summary>
     /// Projects the edge to the specified affine basis.
     /// </summary>
     /// <param name="aBasis">The affine basis to project to.</param>
     /// <returns>The projected edge.</returns>
-    public override BaseSubCP ProjectTo(AffineBasis aBasis) => new SubTwoDimensionalEdge
-      (
-       new SubPoint(first.ProjectTo(aBasis), first, first.Original)
-     , new SubPoint(second.ProjectTo(aBasis), second, second.Original)
-      );
+    public override BaseSubCP ProjectTo(AffineBasis aBasis) {
+      SubPoint first  = Faces.First().Vertices.First();
+      SubPoint second = Faces.Last().Vertices.First();
+
+      return new SubTwoDimensionalEdge
+        (
+         new SubPoint(first.ProjectTo(aBasis), first, first.Original)
+       , new SubPoint(second.ProjectTo(aBasis), second, second.Original)
+        );
+    }
 
   }
 
