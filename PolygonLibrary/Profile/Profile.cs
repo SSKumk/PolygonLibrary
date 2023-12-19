1dd8fc244a85ac6caa11d63ae36fb015f2f22969
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2023-12-19 15:52:01 +0500

    Update Profile.cs

--- PolygonLibrary/Profile/Profile.cs
+++ PolygonLibrary/Profile/Profile.cs
@@ -12,36 +12,36 @@ namespace Profile;
 class Program {
 
   // private static void Sum
-  // static void Main(string[] args) {
-  //   Stopwatch timer = new Stopwatch();
-  //   timer.Restart();
-  //
-  //   // var P = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 4, 100, 1).Select(s => s.ExpandTo(4)));
-  //   // var P = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 4, 100, 1));
-  //   // var Q = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 10, 20, 1));
-  //   // var Q = Simplex4D_FL;
-  //
-  //   var P = GiftWrapping.WrapFaceLattice(Cube5D_list);
-  //   var Q = GiftWrapping.WrapFaceLattice(SimplexRND5D_list);
-  //   Console.WriteLine($"GW = {timer.Elapsed.Milliseconds}");
-  //
-  //   timer.Restart();
-  //   var x = MinkSumSDas(P, Q);
-  //   Console.WriteLine($"MinkSumSDas = {timer.Elapsed.Milliseconds}");
-  //
-  //   timer.Restart();
-  //   var y = MinkSumCH(P, Q);
-  //   Console.WriteLine($"MinkSumCH = {timer.Elapsed.Milliseconds}");
-  //
-  //   if (!x.Equals(y)) { throw new ArgumentException("AAAAAA"); }
-  // }
-  public static void Main(string[] args) {
+  static void Main(string[] args) {
     Stopwatch timer = new Stopwatch();
-    var       swarm = Cube(5,out _, new []{1,2,3,4,5}, 500, null, true);
-    // var swarm = MakePointsOnSphere(5, 5, 10, 1);
     timer.Restart();
-    var x = GiftWrapping.WrapPolytop(swarm);
-    Console.WriteLine($"GW: {timer.ElapsedMilliseconds}");
+
+    // var P = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 4, 100, 1).Select(s => s.ExpandTo(4)));
+    // var P = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 4, 100, 1));
+    // var Q = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 10, 20, 1));
+    // var Q = Simplex4D_FL;
+
+    var P = GiftWrapping.WrapFaceLattice(Cube5D_list);
+    var Q = GiftWrapping.WrapFaceLattice(SimplexRND5D_list);
+    Console.WriteLine($"GW = {timer.Elapsed.Milliseconds}");
+
+    timer.Restart();
+    var x = MinkSumSDas(P, Q);
+    Console.WriteLine($"MinkSumSDas = {timer.Elapsed.Milliseconds}");
+
+    timer.Restart();
+    var y = MinkSumCH(P, Q);
+    Console.WriteLine($"MinkSumCH = {timer.Elapsed.Milliseconds}");
+
+    if (!x.Equals(y)) { throw new ArgumentException("AAAAAA"); }
   }
+  // public static void Main(string[] args) {
+  //   Stopwatch timer = new Stopwatch();
+  //   var       swarm = Cube(5,out _, new []{1,2,3,4,5}, 500, null, true);
+  //   // var swarm = MakePointsOnSphere(5, 5, 10, 1);
+  //   timer.Restart();
+  //   var x = GiftWrapping.WrapPolytop(swarm);
+  //   Console.WriteLine($"GW: {timer.ElapsedMilliseconds}");
+  // }
 
 }
