2e05bf1bb965bcfcea626905d68090be2c2d26ce
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2023-12-19 16:41:59 +0500

    Update Profile.cs

--- PolygonLibrary/Profile/Profile.cs
+++ PolygonLibrary/Profile/Profile.cs
@@ -7,8 +7,7 @@ using static Tests.ToolsTests.TestsPolytopes<DoubleDouble.ddouble, Tests.DDConve
 namespace Profile;
 
 // Всё сломалось!
-// var P = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 4, 100, 1));
-// var Q = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 100, 4, 1));
+
 class Program {
 
   // private static void Sum
@@ -20,21 +19,31 @@ class Program {
     // var P = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 4, 100, 1));
     // var Q = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 10, 20, 1));
     // var Q = Simplex4D_FL;
-
+// var P = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 4, 100, 1));
+// var Q = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 100, 4, 1));
     var P = GiftWrapping.WrapFaceLattice(Cube5D_list);
     var Q = GiftWrapping.WrapFaceLattice(SimplexRND5D_list);
-    Console.WriteLine($"GW = {timer.Elapsed.Milliseconds}");
+    Console.WriteLine($"GW = {timer.ElapsedMilliseconds}");
 
+    const int N = 1;
+
+    FaceLattice? x = null;
     timer.Restart();
-    var x = MinkSumSDas(P, Q);
-    Console.WriteLine($"MinkSumSDas = {timer.Elapsed.Milliseconds}");
+    for (int i = 0; i < N; i++) {
+      x = MinkSumSDas(P, Q);
+    }
+    Console.WriteLine($"MinkSumSDas = {timer.ElapsedMilliseconds}");
 
+    FaceLattice? y = null;
     timer.Restart();
-    var y = MinkSumCH(P, Q);
-    Console.WriteLine($"MinkSumCH = {timer.Elapsed.Milliseconds}");
+    for (int i = 0; i < N; i++) {
+      y = MinkSumCH(P, Q);
+    }
+    Console.WriteLine($"MinkSumCH = {timer.ElapsedMilliseconds}");
 
-    if (!x.Equals(y)) { throw new ArgumentException("AAAAAA"); }
+    // if (!x.Equals(y)) { throw new ArgumentException("AAAAAA"); }
   }
+
   // public static void Main(string[] args) {
   //   Stopwatch timer = new Stopwatch();
   //   var       swarm = Cube(5,out _, new []{1,2,3,4,5}, 500, null, true);
