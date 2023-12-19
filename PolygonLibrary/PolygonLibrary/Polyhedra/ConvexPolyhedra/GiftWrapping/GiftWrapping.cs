4a7da74f12d7926121fbc544656beae01d3636f5
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2023-12-19 09:55:42 +0500

    angle by AngleDouble

--- PolygonLibrary/PolygonLibrary/Polyhedra/ConvexPolyhedra/GiftWrapping/GiftWrapping.cs
+++ PolygonLibrary/PolygonLibrary/Polyhedra/ConvexPolyhedra/GiftWrapping/GiftWrapping.cs
@@ -89,7 +89,7 @@ public partial class Geometry<TNum, TConv>
     /// <returns>The face lattice.</returns>
     private FaceLattice ConstructFL() {
       Dictionary<int, FLNode> allNodes = new Dictionary<int, FLNode>();
-      List<HashSet<FLNode>> lattice = new List<HashSet<FLNode>>();
+      List<HashSet<FLNode>>   lattice  = new List<HashSet<FLNode>>();
       for (int i = 0; i < BuiltPolytop.PolytopDim + 1; i++) {
         lattice.Add(new HashSet<FLNode>());
       }
@@ -155,17 +155,17 @@ public partial class Geometry<TNum, TConv>
     /// <returns>The list of hyperplanes.</returns>
     public List<HyperPlane> HRepresentation {
       get
-      {
-        if (_HRepr is null) {
-          List<HyperPlane> res = new List<HyperPlane>();
-          foreach (Face face in CPolytop.Faces) {
-            res.Add(new HyperPlane(face.Vertices.First(), face.Normal));
+        {
+          if (_HRepr is null) {
+            List<HyperPlane> res = new List<HyperPlane>();
+            foreach (Face face in CPolytop.Faces) {
+              res.Add(new HyperPlane(face.Vertices.First(), face.Normal));
+            }
+            _HRepr = res;
           }
-          _HRepr = res;
-        }
 
-        return _HRepr;
-      }
+          return _HRepr;
+        }
     }
 
     /// <summary>
@@ -191,8 +191,8 @@ public partial class Geometry<TNum, TConv>
 
       SOrig = new HashSet<Point>(Swarm);
       // Переводим рой точек на SubPoints чтобы мы могли возвращаться из-подпространств.
-      HashSet<SubPoint> S = Swarm.Select(s => new SubPoint(s, null, s)).ToHashSet();
-      AffineBasis AffineS = new AffineBasis(S);
+      HashSet<SubPoint> S       = Swarm.Select(s => new SubPoint(s, null, s)).ToHashSet();
+      AffineBasis       AffineS = new AffineBasis(S);
       if (AffineS.SpaceDim < AffineS.VecDim) {
         // Если рой точек образует подпространство размерности меньшей чем размерность самх точек, то
         // уходим в подпространство и там овыпукляем.
@@ -202,7 +202,7 @@ public partial class Geometry<TNum, TConv>
 
       GiftWrappingMain x = new GiftWrappingMain(S);
       BuiltPolytop = x.BuiltPolytop;
-      VPolytop = new VPolytop(x.BuiltPolytop.OriginalVertices);
+      VPolytop     = new VPolytop(x.BuiltPolytop.OriginalVertices);
     }
 
     /// <summary>
@@ -210,13 +210,13 @@ public partial class Geometry<TNum, TConv>
     /// </summary>
     private sealed class GiftWrappingMain {
 
-      #region Internal fields for GW algorithm
+#region Internal fields for GW algorithm
       private readonly HashSet<SubPoint> S;        // Рой точек
-      private readonly int spaceDim; // Размерность пространства, в котором происходит овыпукление
-      private BaseSubCP? initFace; // Грань, с которой будем перекатываться в поисках других граней.
+      private readonly int               spaceDim; // Размерность пространства, в котором происходит овыпукление
+      private          BaseSubCP?        initFace; // Грань, с которой будем перекатываться в поисках других граней.
 
-      private readonly HashSet<BaseSubCP> buildFaces = new HashSet<BaseSubCP>(); // Копим грани текущей размерности.
-      private readonly HashSet<SubPoint> buildPoints = new HashSet<SubPoint>();  // Копим точки для создаваемого многогранника
+      private readonly HashSet<BaseSubCP> buildFaces  = new HashSet<BaseSubCP>(); // Копим грани текущей размерности.
+      private readonly HashSet<SubPoint>  buildPoints = new HashSet<SubPoint>();  // Копим точки для создаваемого многогранника
 
       private readonly TempIncidenceInfo
         buildIncidence = new TempIncidenceInfo(); // ребро --> (F1, F2) которые соседствуют через это ребро
@@ -226,19 +226,19 @@ public partial class Geometry<TNum, TConv>
       /// about (d-1)-faces, vertices and information about all k-faces, 0 &lt; k &lt; d - 1.
       /// </summary>
       public readonly BaseSubCP BuiltPolytop;
-      #endregion
+#endregion
 
-      #region Constructors
+#region Constructors
       public GiftWrappingMain(HashSet<SubPoint> Swarm, BaseSubCP? initFace = null) {
-        S = Swarm;
-        spaceDim = S.First().Dim;
+        S             = Swarm;
+        spaceDim      = S.First().Dim;
         this.initFace = initFace;
 
         BuiltPolytop = GW();
       }
-      #endregion
+#endregion
 
-      #region Main functions
+#region Main functions
       /// <summary>
       /// Executes the gift wrapping algorithm.
       /// </summary>
@@ -264,7 +264,7 @@ public partial class Geometry<TNum, TConv>
         buildFaces.Add(initFace);
         buildPoints.UnionWith(initFace.Vertices);
 
-        #region Debug
+#region Debug
         Debug.Assert(initFace is not null, $"GiftWrapping.GW (space dim = {spaceDim}): initial facet is null!");
         Debug.Assert(initFace.Normal is not null, $"GiftWrapping.GW (space dim = {spaceDim}): initial facet is null.");
         Debug.Assert(!initFace.Normal.IsZero, $"GiftWrapping.GW (space dim = {spaceDim}): initial facet has zero length.");
@@ -288,7 +288,7 @@ public partial class Geometry<TNum, TConv>
            initFace.Faces!.All(F => F.PolytopDim == spaceDim - 2)
          , $"GiftWrapping.GW (space dim = {spaceDim}): The dimension of all edges of initFace must equal to d-2!"
           );
-        #endregion
+#endregion
 
         // Будем складывать сюда грани, с которых потенциально можно перекатиться
         Queue<BaseSubCP> toTreat = new Queue<BaseSubCP>();
@@ -351,37 +351,40 @@ public partial class Geometry<TNum, TConv>
         Debug.Assert(S.Count != 0, $"BuildInitialPlaneSwart (dim = {spaceDim}): The swarm must has at least one point!");
 
         // Для построения начальной плоскости найдём точку самую малую в лексикографическом порядке. (левее неё уже точек нет)
-        SubPoint origin = S.Min()!;
+        SubPoint    origin = S.Min()!;
         AffineBasis FinalV = new AffineBasis(origin);
 
         // нормаль к плоскости начальной
         Vector n = -Vector.CreateOrth(spaceDim, 1);
 
         while (FinalV.SpaceDim < spaceDim - 1) {
-          TNum maxAngle = -Tools.Six; // "Большое отрицательное число."
-          SubPoint? sExtr = null;
+          // TNum      maxAngle = -Tools.Six; // "Большое отрицательное число."
+          double    maxAngle = -10.0;
+          SubPoint? sExtr    = null;
 
-          Vector e;
-          int i = 0;
+          Vector   e;
+          int      i      = 0;
           Vector[] nBasis = { n };
           do {
             i++;
             e = Vector.OrthonormalizeAgainstBasis(Vector.CreateOrth(spaceDim, i), FinalV.Basis, nBasis);
           } while (e.IsZero && i <= spaceDim);
-          Debug.Assert(i <= spaceDim, $"BuildInitialPlaneSwart (dim = {spaceDim}): Can't find vector e! That orthogonal to FinalV and n.");
+          Debug.Assert
+            (i <= spaceDim, $"BuildInitialPlaneSwart (dim = {spaceDim}): Can't find vector e! That orthogonal to FinalV and n.");
 
-                         // Vector? r = null; нужен для процедуры Сварта (ниже)
+          // Vector? r = null; нужен для процедуры Сварта (ниже)
           foreach (SubPoint s in S) {
             // вычисляем "кандидата" проецируя в плоскость (e,n)
             Vector u = (s - origin).ProjectToPlane(e, n);
 
             if (!u.IsZero) {
-              TNum angle = Vector.Angle(e, u);
-
+              // TNum   angle = Vector.Angle(e, u);
+              double angle = Vector.AngleDouble(e, u); // в ddouble Acos очень медленная операция. Поэтому считаем в double-ах.
               // Кандидата с самым большим углом запоминаем
-              if (Tools.GT(angle, maxAngle)) {
+              // if (Tools.GT(angle, maxAngle)) {
+                if (angle - maxAngle > Tools.EpsDouble) {
                 maxAngle = angle;
-                sExtr = s;
+                sExtr    = s;
                 // r = u.Normalize();
               }
             }
@@ -495,24 +498,26 @@ public partial class Geometry<TNum, TConv>
 
         // v вектор перпендикулярный ребру и лежащий в текущей плоскости
         AffineBasis edgeBasis = new AffineBasis(edge.Vertices);
-        SubPoint f = face.Vertices.First(p => !edge.Vertices.Contains(p));
-        Vector v = Vector.OrthonormalizeAgainstBasis(f - edgeBasis.Origin, edgeBasis.Basis);
+        SubPoint    f         = face.Vertices.First(p => !edge.Vertices.Contains(p));
+        Vector      v         = Vector.OrthonormalizeAgainstBasis(f - edgeBasis.Origin, edgeBasis.Basis);
 
-        Vector? r = null;
-        SubPoint? sStar = null;
-        TNum maxAngle = -Tools.Six; // Something small
+        Vector?   r        = null;
+        SubPoint? sStar    = null;
+        // TNum      maxAngle = -Tools.Six; // Something small
+        double    maxAngle = -10.0;
 
         // ищем вектор u такой, что в плоскости (v,N) угол между ним и v наибольший, где N нормаль к текущей плоскости
         foreach (SubPoint s in S) {
-          // Vector u = (so * v) * v + (so * face.Normal) * face.Normal;
           Vector u = (s - edgeBasis.Origin).ProjectToPlane(v, face.Normal);
           if (!u.IsZero) {
-            TNum angle = Vector.Angle(v, u);
+            // TNum   angle = Vector.Angle(v, u);
+            double angle = Vector.AngleDouble(v, u);
 
-            if (Tools.GT(angle, maxAngle)) {
+            // if (Tools.GT(angle, maxAngle)) {
+              if (angle - maxAngle > Tools.EpsDouble) {
               maxAngle = angle;
-              r = u.Normalize();
-              sStar = s;
+              r        = u.Normalize();
+              sStar    = s;
             }
           }
         }
@@ -540,9 +545,9 @@ public partial class Geometry<TNum, TConv>
 
         return BuildFace(newF_aBasis, n, r, edge);
       }
-      #endregion
+#endregion
 
-      #region Auxiliary functions
+#region Auxiliary functions
       /// <summary>
       /// Calculates the outer normal vector of a given set of points.
       /// </summary>
@@ -550,7 +555,7 @@ public partial class Geometry<TNum, TConv>
       /// <returns>The outer normal vector.</returns>
       private Vector CalcOuterNormal(AffineBasis planeBasis) {
         HyperPlane hp = new HyperPlane(planeBasis);
-        Vector n = hp.Normal;
+        Vector     n  = hp.Normal;
         OrientNormal(ref n, planeBasis.Origin);
 
 #if DEBUG
@@ -583,7 +588,7 @@ public partial class Geometry<TNum, TConv>
           }
         }
       }
-      #endregion
+#endregion
 
     }
 
