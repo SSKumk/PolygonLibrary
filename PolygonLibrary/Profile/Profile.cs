6074ab78c475f6e95981e9efc91e5751c128c6c3
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-01-11 20:03:11 +0500

    Update Profile.cs

--- PolygonLibrary/Profile/Profile.cs
+++ PolygonLibrary/Profile/Profile.cs
@@ -1,32 +1,49 @@
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using DoubleDouble;
-// using static CGLibrary.Geometry<double, Tests.DConvertor>;
-// using static Tests.ToolsTests.TestsPolytopes<double, Tests.DConvertor>;
+using Tests.ToolsTests;
 using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
 using static Tests.ToolsTests.TestsPolytopes<DoubleDouble.ddouble, Tests.DDConvertor>;
+using static Tests.ToolsTests.TestsBase<DoubleDouble.ddouble, Tests.DDConvertor>;
 
 namespace Profile;
+  // const int    N      = 100;  // 1) Кубы по размерностям без дополнительных точек
+  // GiftWrapping? Polytop = null;
+  // for (int i = 4; i <= 4; i++) {
+  //   // var       S  = Cube_list(i);
+  //   var       S  = Sphere_list(i, 2, 100, 1);
+  //   Stopwatch timer = new Stopwatch();
+  //   timer.Restart();
+  //   for (int k = 0; k < N; k++) { Polytop = new GiftWrapping(S); }
+  //   timer.Stop();
+  //   Console.WriteLine($@"{i} & {timer.Elapsed.TotalSeconds / N :F5} \\");
+  // }
+  //   Polytop.Equals(null);
 
-// Всё сломалось!
-// var P = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 4, 100, 1));
-// var Q = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 100, 4, 1));
 class Program {
 
   static void Main(string[] args) {
+    CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+  // ProfileMinkSum();
+  var x = GenLinearBasis(5);
+  }
+
+  private static void ProfileMinkSum() {
     Stopwatch timer = new Stopwatch();
     timer.Restart();
 
-    // var P = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 4, 100, 1).Select(s => s.ExpandTo(4)));
-    // var P = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 4, 100, 1));
-    // var Q = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 10, 20, 1));
+    // var P = GiftWrapping.WrapFaceLattice(Sphere_list(3, 4, 100, 1).Select(s => s.ExpandTo(4)));
+    // var P = GiftWrapping.WrapFaceLattice(Sphere_list(3, 4, 100, 1));
+    // var Q = GiftWrapping.WrapFaceLattice(Sphere_list(3, 100, 4, 1));
     // var Q = Simplex4D_FL;
-    var P = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 10, 10, 1));
-    var Q = GiftWrapping.WrapFaceLattice(MakePointsOnSphere(3, 20, 4, 1));
-    // var P = GiftWrapping.WrapFaceLattice(Cube5D_list);
-    // var Q = GiftWrapping.WrapFaceLattice(SimplexRND5D_list);
+    // var P = GiftWrapping.WrapFaceLattice(Sphere_list(3, 10, 10, 1));
+    // var Q = GiftWrapping.WrapFaceLattice(Sphere_list(3, 20, 4, 1));
+    var P = GiftWrapping.WrapFaceLattice(Cube5D_list);
+    var Q = GiftWrapping.WrapFaceLattice(SimplexRND5D_list);
     Console.WriteLine($"GW = {timer.ElapsedMilliseconds}");
 
-    const int N = 10;
+    const int N = 100;
 
     FaceLattice? x = null;
     timer.Restart();
@@ -44,16 +61,5 @@ class Program {
 
     if (!x!.Equals(y)) { throw new ArgumentException("AAAAAA"); }
   }
-
-
-  // public static void Main(string[] args) {
-  //   Stopwatch timer = new Stopwatch();
-  //   var       swarm = Cube(5,out _, new []{1,2,3,4,5}, 500, null, true);
-  //   // var swarm = MakePointsOnSphere(5, 5, 10, 1);
-  //   timer.Restart();
-  //   var x = GiftWrapping.WrapPolytop(swarm);
-  //   Console.WriteLine($"GW: {timer.ElapsedMilliseconds}");
-  //   Console.WriteLine(x.Vertices.Count);
-  // }
 
 }
