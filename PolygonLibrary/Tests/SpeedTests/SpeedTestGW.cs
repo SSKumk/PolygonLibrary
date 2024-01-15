df912dfb401193668c021acc0e39401e3090d9d8
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-01-15 21:37:10 +0500

    Update SpeedTestGW.cs

--- PolygonLibrary/Tests/SpeedTests/SpeedTestGW.cs
+++ PolygonLibrary/Tests/SpeedTests/SpeedTestGW.cs
@@ -3,10 +3,11 @@ using System.Globalization;
 using CGLibrary;
 using NUnit.Framework;
 using DoubleDouble;
-// using static CGLibrary.Geometry<double, Tests.DConvertor>;
-// using static Tests.ToolsTests.TestsBase<double, Tests.DConvertor>;
-// using static Tests.ToolsTests.TestsPolytopes<double, Tests.DConvertor>;
+using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
+using static Tests.ToolsTests.TestsBase<DoubleDouble.ddouble, Tests.DDConvertor>;
+using static Tests.ToolsTests.TestsPolytopes<DoubleDouble.ddouble, Tests.DDConvertor>;
 using System.Numerics;
+using NUnit.Framework.Internal;
 using Tests.ToolsTests;
 
 
@@ -17,50 +18,50 @@ namespace Tests.SpeedTests;
 [TestFixture]
 public class SpeedTestGW {
 
-  [Test]
-  public void Circle2D_Double() { SphereBench<double, DConvertor>(2, 1, (int)1e6, "Circle2D-double"); }
+  // [Test]
+  // public void Circle2D_Double() { SphereBench<double, DConvertor>(2, 1, (int)1e6, "Circle2D-double"); }
 
   [Test]
   public void Circle2D_DoubleDouble() { SphereBench<ddouble, DDConvertor>(2, 1, (int)1e6, "Circle2D-double-double"); }
 
-  [Test]
-  public void Sphere3D_Double() { SphereBench<double, DConvertor>(3, 2, (int)1e3, "Sphere3D-double"); }
+  // [Test]
+  // public void Sphere3D_Double() { SphereBench<double, DConvertor>(3, 2, (int)1e3, "Sphere3D-double"); }
 
   [Test]
   public void Sphere3D_DoubleDouble() { SphereBench<ddouble, DDConvertor>(3, 2, (int)1e3, "Sphere3D-double-double"); }
 
-  [Test]
-  public void Sphere4D_Double() { SphereBench<double, DConvertor>(4, 2, (int)1e3, "Sphere4D-double"); }
+  // [Test]
+  // public void Sphere4D_Double() { SphereBench<double, DConvertor>(4, 2, (int)1e3, "Sphere4D-double"); }
 
   [Test]
   public void Sphere4D_DoubleDouble() { SphereBench<ddouble, DDConvertor>(4, 2, (int)1e3, "Sphere4D-double-double"); }
 
-  [Test]
-  public void Sphere5D_Double() { SphereBench<double, DConvertor>(5, 2, (int)1e3, "Sphere5D-double"); }
+  // [Test]
+  // public void Sphere5D_Double() { SphereBench<double, DConvertor>(5, 2, (int)1e3, "Sphere5D-double"); }
 
   [Test]
   public void Sphere5D_DoubleDouble() { SphereBench<ddouble, DDConvertor>(5, 2, (int)1e3, "Sphere5D-double-double"); }
 
-  [Test]
-  public void Cube2D_Double() { CubeBench<double, DConvertor>(2, (int)1e6, "Cube2D-double"); }
+  // [Test]
+  // public void Cube2D_Double() { CubeBench<double, DConvertor>(2, (int)1e6, "Cube2D-double"); }
 
   [Test]
   public void Cube2D_DoubleDouble() { CubeBench<ddouble, DDConvertor>(2, (int)1e6, "Cube2D-double-double"); }
 
-  [Test]
-  public void Cube3D_Double() { CubeBench<double, DConvertor>(3, (int)1e5, "Cube3D-double"); }
+  // [Test]
+  // public void Cube3D_Double() { CubeBench<double, DConvertor>(3, (int)1e5, "Cube3D-double"); }
 
   [Test]
   public void Cube3D_DoubleDouble() { CubeBench<ddouble, DDConvertor>(3, (int)1e5, "Cube3D-double-double"); }
 
-  [Test]
-  public void Cube4D_Double() { CubeBench<double, DConvertor>(4, (int)1e5, "Cube4D-double"); }
+  // [Test]
+  // public void Cube4D_Double() { CubeBench<double, DConvertor>(4, (int)1e5, "Cube4D-double"); }
 
   [Test]
   public void Cube4D_DoubleDouble() { CubeBench<ddouble, DDConvertor>(4, (int)1e5, "Cube4D-double-double"); }
 
-  [Test]
-  public void Cube5D_Double() { CubeBench<double, DConvertor>(5, (int)1e5, "Cube5D-double"); }
+  // [Test]
+  // public void Cube5D_Double() { CubeBench<double, DConvertor>(5, (int)1e5, "Cube5D-double"); }
 
   [Test]
   public void Cube5D_DoubleDouble() { CubeBench<ddouble, DDConvertor>(5, (int)1e5, "Cube5D-double-double"); }
@@ -75,16 +76,24 @@ public class SpeedTestGW {
   public void Cube8D_DoubleDouble() { CubeBench<ddouble, DDConvertor>(8, (int)1e5, "Cube8D-double-double"); }
 
   [Test]
+  public void Cubes_DoubleDouble() {
+    for (int i = 7; i <= 8; i++) {
+      CubeBench<ddouble, DDConvertor>(i, (int)1e5, $"Cube{i}D");
+    }
+  }
+
+  [Test]
   public void SimplicesRND_DoubleDouble() {
     for (int i = 2; i <= 8; i++) {
-      SimplexRNDBench<ddouble, DDConvertor>(i, (int)1e5, $"SimplexRND{i}D-double-double");
+      SimplexRNDBench<ddouble, DDConvertor>(i, (int)1e5, $"SimplexRND{i}D");
     }
   }
 
   [Test]
   public void Spheres_DoubleDouble() {
-    for (int i = 6; i <= 6; i++) {
-      SphereBench<ddouble, DDConvertor>(i, 2,(int)1e3, $"Sphere{i}D-double-double");
+    const int theta = 2;
+    for (int i = 8; i <= 8; i++) {
+      SphereBench<ddouble, DDConvertor>(i, theta, (int)1e2, $"Sphere{i}D-{theta}-n-1");
     }
   }
 
@@ -95,6 +104,7 @@ public class SpeedTestGW {
     Stopwatch          timer  = new Stopwatch();
     using StreamWriter writer = new StreamWriter(Directory.GetCurrentDirectory() + "/SpeedBench/" + fileName + ".txt");
 
+    const int N = 10;
     // Берём окружность и начинаем туда напихивать точки
     for (int n = 10; n <= maxAmount; n *= 10) {
       // writer.WriteLine($"n = {n,-8}");
@@ -103,19 +113,17 @@ public class SpeedTestGW {
       // var circleSmall = TestsPolytopes<TNum, TConv>.Sphere_list(dim, thetaPoints, k, TConv.FromDouble(0.5));
       var circle = TestsPolytopes<TNum, TConv>.Sphere_list(dim, thetaPoints, n, TConv.FromInt(1));
       // circle.AddRange(circleSmall);
+      Geometry<TNum, TConv>.GiftWrapping? Circle = null;
       timer.Restart();
-      Geometry<TNum, TConv>.GiftWrapping Circle = new Geometry<TNum, TConv>.GiftWrapping(circle);
+      for (int i = 0; i < N; i++) { Circle = new Geometry<TNum, TConv>.GiftWrapping(circle); }
       timer.Stop();
-
 
       writer.WriteLine
         (
-         // $"    k = {k,-8} sec: {timer.Elapsed.TotalSeconds.ToString("F5", CultureInfo.InvariantCulture),-12}" +
-         $"n = {n,-8} sec: {timer.Elapsed.TotalSeconds.ToString("F5", CultureInfo.InvariantCulture),-12}" +
-         $" Total: {circle.Count,-8} inCH = {Circle.Vertices.Count}"
+         $"n = {n,-8} sec: {(timer.Elapsed.TotalSeconds / N).ToString("F5", CultureInfo.InvariantCulture),-12}" +
+         $" Total: {circle.Count,-8} inCH = {Circle!.Vertices.Count}"
         );
       writer.Flush();
-      // }
     }
   }
 
@@ -126,7 +134,7 @@ public class SpeedTestGW {
     Stopwatch    timer  = new Stopwatch();
     StreamWriter writer = new StreamWriter(Directory.GetCurrentDirectory() + "/SpeedBench/" + fileName + ".txt");
 
-    for (int k = 1000; k <= maxAmount; k *= 10) {
+    for (int k = 1; k <= maxAmount; k *= 10) {
       var cube = TestsPolytopes<TNum, TConv>.Cube(dim, out _, new int[] { dim }, k);
       timer.Restart();
       var Cube = new Geometry<TNum, TConv>.GiftWrapping(cube);
@@ -159,6 +167,31 @@ public class SpeedTestGW {
          $" Total: {simplexRND.Count,-8} inCH = {Simplex.Vertices.Count}"
         );
       writer.Flush();
+    }
+  }
+
+  [Test]
+  public void CubesWithPointsOnFaces() {
+    const int nPoints = 100;
+    const int N       = 5;
+
+    Stopwatch timer = new Stopwatch();
+    CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+    for (int dim = 3; dim <= 6; dim++) {
+      StreamWriter writer = new StreamWriter
+        (Directory.GetCurrentDirectory() + "/SpeedBench/" + $"Cube{dim}D-withPoints" + ".txt");
+      List<int> fIDs = Enumerable.Range(1, dim).ToList();
+
+      for (int n = 1; n <= nPoints; n *= 10) {
+        List<Point>   S = RotateRND(Cube(dim, out _, fIDs, n, 255, true));
+        GiftWrapping? P = null;
+        timer.Restart();
+        for (int i = 0; i < N; i++) { P = new GiftWrapping(S); }
+        timer.Stop();
+        writer.WriteLine($"{P!.FaceLattice.NonZeroKFacesAmount}-{S.Count, -5} & {timer.Elapsed.TotalSeconds / N,-8:F5}");
+        writer.Flush();
+      }
     }
   }
 
