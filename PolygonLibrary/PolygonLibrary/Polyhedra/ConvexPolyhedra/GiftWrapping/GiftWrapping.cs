045408522fd3c9e1223e6302ca2c539a0a3419f6
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-02-23 11:47:11 +0500

    S.Count() <= 1 --> exeption

--- PolygonLibrary/PolygonLibrary/Polyhedra/ConvexPolyhedra/GiftWrapping/GiftWrapping.cs
+++ PolygonLibrary/PolygonLibrary/Polyhedra/ConvexPolyhedra/GiftWrapping/GiftWrapping.cs
@@ -173,7 +173,7 @@ public partial class Geometry<TNum, TConv>
     /// <param name="Swarm">The swarm of points to convexify.</param>
     public GiftWrapping(IEnumerable<Point> Swarm) {
       // Пока не будет SubBaseCP размерности 0.
-      if (Swarm.Count() == 1) {
+      if (Swarm.Count() <= 1) {
         throw new ArgumentException("GW: At least 2 points must be in Swarm for convexification.");
       }
 
