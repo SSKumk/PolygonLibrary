72e82ad189040698a8f90560c5e4651e5353caa8
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-03-09 13:36:39 +0500

    +

--- PolygonLibrary/Profile/Profile.cs
+++ PolygonLibrary/Profile/Profile.cs
@@ -4,17 +4,23 @@ using System.Globalization;
 using System.Runtime.InteropServices;
 using DoubleDouble;
 using Tests.ToolsTests;
-using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
 using static Tests.ToolsTests.TestsPolytopes<DoubleDouble.ddouble, Tests.DDConvertor>;
 using static Tests.ToolsTests.TestsBase<DoubleDouble.ddouble, Tests.DDConvertor>;
+using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
+
+// using static CGLibrary.Geometry<double, Tests.DConvertor>;
 
 namespace Profile;
 
 class Program {
 
+  private static readonly string pathData =
+    "F:/Works/IMM/Аспирантура/_PolygonLibrary/PolygonLibrary/Tests/OtherTests/LDG_computations/";
+
   static void Main(string[] args) {
     CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
-
+    SolverLDG ldg = new SolverLDG(pathData, "Spheres3D");
+    ldg.Solve(true);
   }
 
 }
