89a83978e204797be066db115781eb0ba8c5d3f8
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-03-13 21:47:47 +0500

    Update Sandbox.cs

--- PolygonLibrary/Tests/OtherTests/Sandbox.cs
+++ PolygonLibrary/Tests/OtherTests/Sandbox.cs
@@ -6,6 +6,7 @@ using static CGLibrary.Geometry<double, Tests.DConvertor>;
 using static Tests.ToolsTests.TestsPolytopes<double, Tests.DConvertor>;
 using static Tests.ToolsTests.TestsBase<double, Tests.DConvertor>;
 using System.IO;
+
 namespace Tests.OtherTests;
 
 [TestFixture]
@@ -13,10 +14,17 @@ public class Sandbox {
 
   [Test]
   public void MakePointsOnSphereTest() {
-    var x = Sphere_list(3, 2, 100, 1);
-    var y = new GiftWrapping(x);
-    // var y = MakePointsOnSphere_3D(3, 4);
+    ConvexPolytop
+     .DistanceToBall_2
+        (
+         2
+       , 40
+       , 40
+       , 1
+       , 3
+        )
+     .WriteTXT_3D("F:/Works/IMM/Аспирантура/_PolygonLibrary/PolygonLibrary/Tests/OtherTests/LDG_computations/Other/DistanceToBall_2-sphere");
+    // .WriteTXT_3D_forDasha("F:/Works/IMM/Аспирантура/_PolygonLibrary/PolygonLibrary/Tests/OtherTests/LDG_computations/Other/Cube3D");
   }
+
 }
-
-
