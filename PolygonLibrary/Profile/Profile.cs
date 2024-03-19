1415b7eac255341ecf9afd0fa3c8630a622127e9
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-03-19 11:52:13 +0500

    Update Profile.cs

--- PolygonLibrary/Profile/Profile.cs
+++ PolygonLibrary/Profile/Profile.cs
@@ -19,22 +19,15 @@ class Program {
 
   static void Main(string[] args) {
     // CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
-    // var         s4      = ConvexPolytop.Sphere(4, 10, 12, Vector.Zero(4), 1);
-    // AffineBasis aBasis3 = new AffineBasis(Vector.Zero(4),new LinearBasis(3,4));
-    // var         s3      = ConvexPolytop.AsVPolytop(aBasis3.ProjectPoints(s4.Vertices).ToHashSet(),true);
-    // s3.WriteTXT_3D(pathData + "s4proj_s3");
 
-    // ConvexPolytop s4_1 = ConvexPolytop.Sphere(4,10,20);
+    // SolverLDG.WriteSimplestTask_TerminalSet_GameItself(3, pathData);
+    // SolverLDG solverLdg = new SolverLDG(pathData, "simplestGame");
+    // solverLdg.Solve(true);
 
-    // SolverLDG ldg = new SolverLDG(pathData, "Spheres3D");
-    // ldg.Solve(true);
-
-    // ConvexPolytop.Sphere(5, 20, 30, Vector.Zero(5), 1);
-    SolverLDG.WriteSimplestFile(3, pathData);
-    SolverLDG solverLdg = new SolverLDG(pathData, "simplest");
+    SolverLDG.WriteSimplestTask_Payoff_Supergraphic_2D(pathData);
+    SolverLDG solverLdg = new SolverLDG(pathData, "simplestSupergraphic");
     solverLdg.Solve(true);
 
-    // ConvexPolytop x = ConvexPolytop.AsVPolytop(new HashSet<Vector>() { Vector.Ones(3) });
   }
 
 }
