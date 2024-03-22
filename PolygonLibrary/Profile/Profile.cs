86e9a1782ae6e892b7375da9f38a661631191802
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-03-22 13:39:19 +0500

    Update Profile.cs

--- PolygonLibrary/Profile/Profile.cs
+++ PolygonLibrary/Profile/Profile.cs
@@ -18,23 +18,7 @@ class Program {
     "F:/Works/IMM/Аспирантура/_PolygonLibrary/PolygonLibrary/Tests/OtherTests/LDG_computations/";
 
   static void Main(string[] args) {
-    // CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
-
-    // SolverLDG.WriteSimplestTask_TerminalSet_GameItself(3, pathData);
-    // SolverLDG solverLdg1 = new SolverLDG(pathData, "simplestGame");
-    // solverLdg1.Solve(true);
-    // //
-    // SolverLDG.WriteSimplestTask_Payoff_Supergraphic_2D(pathData);
-    SolverLDG solverLdg2 = new SolverLDG(pathData, "simplestSupergraphic");
-    solverLdg2.Solve(true);
-
-    // var sec0_2 = solverLdg2.GetSection(0.2);
-    // var slice  = ConvexPolytop.SectionByHyperPlane(sec0_2, new HyperPlane(Vector.Ones(3), Vector.Ones(3)));
-
-    // slice.WriteTXT_3D(pathData + "Other/slice");
-
-    // todo ПРОТЕСТИРОВАТЬ различные сочетания GoalType и MType !!!
-
+    CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
     // SolverLDG solverLdg = new SolverLDG(pathData, "materialDot1-1-supG");
     // SolverLDG solverLdg = new SolverLDG(pathData, "materialDot1-0.9-supG");
@@ -42,7 +26,16 @@ class Program {
     // SolverLDG solverLdg = new SolverLDG(pathData, "Spheres3D");
     // SolverLDG solverLdg = new SolverLDG(pathData, "oscillator1-0.9-supG");
 
+    ConvexPolytop x = ConvexPolytop.ReadAsPoints
+      (
+       pathData +
+       "Ep_MaterialDot-1-0.9_P#RectParallel_Q#RectParallel_M#DtnOrigin_Ball_2-T4-P100_-CMax2/5.80)materialDot1-0.9-supG"
+      );
 
+// x.WriteTXT_3D(
+//               pathData +
+//               "Ep_MaterialDot-1-0.9_P#RectParallel_Q#RectParallel_M#DtnOrigin_Ball_2-T4-P100_-CMax2/__7.00)materialDot1-0.9-supG"
+//              );
     // solverLdg.Solve(true);
   }
 
