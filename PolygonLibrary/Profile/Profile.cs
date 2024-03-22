a47971d53ebfff8c309b97f5e48dfc7573fe6971
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-03-22 15:14:00 +0500

    Update Profile.cs

--- PolygonLibrary/Profile/Profile.cs
+++ PolygonLibrary/Profile/Profile.cs
@@ -21,22 +21,13 @@ class Program {
     CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
     // SolverLDG solverLdg = new SolverLDG(pathData, "materialDot1-1-supG");
-    // SolverLDG solverLdg = new SolverLDG(pathData, "materialDot1-0.9-supG");
+    SolverLDG solverLdg = new SolverLDG(pathData, "materialDot1-0.9-supG");
     // SolverLDG solverLdg = new SolverLDG(pathData, "oscillator1-1-supG");
     // SolverLDG solverLdg = new SolverLDG(pathData, "Spheres3D");
     // SolverLDG solverLdg = new SolverLDG(pathData, "oscillator1-0.9-supG");
 
-    ConvexPolytop x = ConvexPolytop.ReadAsPoints
-      (
-       pathData +
-       "Ep_MaterialDot-1-0.9_P#RectParallel_Q#RectParallel_M#DtnOrigin_Ball_2-T4-P100_-CMax2/5.80)materialDot1-0.9-supG"
-      );
 
-// x.WriteTXT_3D(
-//               pathData +
-//               "Ep_MaterialDot-1-0.9_P#RectParallel_Q#RectParallel_M#DtnOrigin_Ball_2-T4-P100_-CMax2/__7.00)materialDot1-0.9-supG"
-//              );
-    // solverLdg.Solve(true);
+    solverLdg.Solve(true);
   }
 
 }
