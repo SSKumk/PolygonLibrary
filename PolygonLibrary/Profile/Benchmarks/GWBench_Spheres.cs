90f3625b9b98e4f6cdebf08101413fa1636a2cb7
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-04-04 23:28:38 +0500

    +

--- PolygonLibrary/Profile/Benchmarks/GWBench_Spheres.cs
+++ PolygonLibrary/Profile/Benchmarks/GWBench_Spheres.cs
@@ -13,8 +13,8 @@ using static Geometry<ddouble, Tests.DDConvertor>;
 using static Tests.ToolsTests.TestsPolytopes<ddouble, Tests.DDConvertor>;
 
 [ShortRunJob]
-[WarmupCount(1)]
-[IterationCount(1)]
+// [WarmupCount(1)]
+// [IterationCount(1)]
 public class GWBenchSpheres {
 
   // [Params(3, 4, 5, 6)]
@@ -28,11 +28,11 @@ public class GWBenchSpheres {
   // // ReSharper disable once UnassignedField.Global
   // public int amount;
 
-  [Params( 10, 18, 36)]
+  [Params( 10, 18)]
   // ReSharper disable once UnassignedField.Global
   public int thetaPartition;
 
-  [Params(10, 16, 32)]
+  [Params(10, 16)]
   // ReSharper disable once UnassignedField.Global
   public int phiPartition;
 
@@ -45,6 +45,20 @@ public class GWBenchSpheres {
 /*
 Заворачиваем сферы с дополнительными точками
 
+| Method   | dim | thetaPartition | phiPartition | Mean       | Error |
+|--------- |---- |--------------- |------------- |-----------:|------:|
+| GWSphere | 3   | 10             | 10           |   0.0222 s |    NA |
+| GWSphere | 3   | 10             | 16           |   0.0526 s |    NA |
+| GWSphere | 3   | 18             | 10           |   0.0671 s |    NA |
+| GWSphere | 3   | 18             | 16           |   0.1601 s |    NA |
+| GWSphere | 3   | 36             | 10           |   0.2446 s |    NA |
+| GWSphere | 3   | 36             | 16           |   0.6793 s |    NA |
+| GWSphere | 4   | 10             | 10           |   2.5352 s |    NA |
+| GWSphere | 4   | 10             | 16           |   6.1830 s |    NA |
+| GWSphere | 4   | 18             | 10           |  26.8099 s |    NA |
+| GWSphere | 4   | 18             | 16           |  57.5789 s |    NA |
+| GWSphere | 4   | 36             | 10           | 361.7732 s |    NA |
+| GWSphere | 4   | 36             | 16           | 924.0218 s |    NA |
 
  */
 
