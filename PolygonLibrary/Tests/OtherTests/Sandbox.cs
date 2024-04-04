c419455b0780002c6afc7e50292da378dadaab91
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-04-04 16:02:45 +0500

    Update Sandbox.cs

--- PolygonLibrary/Tests/OtherTests/Sandbox.cs
+++ PolygonLibrary/Tests/OtherTests/Sandbox.cs
@@ -2,11 +2,12 @@ using System.Diagnostics;
 using System.Globalization;
 using CGLibrary;
 using NUnit.Framework;
-using static Tests.ToolsTests.TestsPolytopes<double, Tests.DConvertor>;
-using static Tests.ToolsTests.TestsBase<double, Tests.DConvertor>;
+using static Tests.ToolsTests.TestsPolytopes<DoubleDouble.ddouble, Tests.DDConvertor>;
+using static Tests.ToolsTests.TestsBase<DoubleDouble.ddouble, Tests.DDConvertor>;
 using System.IO;
 using DoubleDouble;
 using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
+
 
 // using static CGLibrary.Geometry<double, Tests.DConvertor>;
 
@@ -19,24 +20,27 @@ public class Sandbox {
   private static readonly string pathData =
     "F:/Works/IMM/Аспирантура/_PolygonLibrary/PolygonLibrary/Tests/OtherTests/LDG_computations/Other/";
 
+  private static readonly string pathPresentationFig =
+    "F:\\Works\\Конференции и семинары\\2024\\04. Молодёжный семинар\\Figures\\";
+
   [Test]
   public void Sandboxx() {
-    // HyperPlane hp = new HyperPlane(Vector.Ones(4), 0.5);
-    // var        x  = ConvexPolytop.SectionByHyperPlane(ConvexPolytop.RectParallel(Vector.Zero(4), Vector.Ones(4)), hp);
-    // .Ellipsoid(3, 10, 20, Vector.Zero(3), new Vector(new ddouble[] { 4.5, 6, 3 }))
-    // .WriteTXT_3D(pathData + "Ellipsoid-4.5-6-3");
-    // x.WriteTXT_3D(pathData + "Cube4D-Section");
-    // ConvexPolytop
-    //  .DistanceToBall_2
-    //     (
-    //      2
-    //    , 40
-    //    , 40
-    //    , 1
-    //    , 3
-    //     )
-    //  .WriteTXT_3D("F:/Works/IMM/Аспирантура/_PolygonLibrary/PolygonLibrary/Tests/OtherTests/LDG_computations/Other/DistanceToBall_2-sphere");
-    // .WriteTXT_3D_forDasha("F:/Works/IMM/Аспирантура/_PolygonLibrary/PolygonLibrary/Tests/OtherTests/LDG_computations/Other/Cube3D");
+    ConvexPolytop ellips = ConvexPolytop.Ellipsoid(3, 20, 100, Vector.Zero(3), new Vector(new ddouble[] { 1, 2, 3 }));
+    ConvexPolytop cube   = ConvexPolytop.AsVPolytop(Cube(3, out _, new int[] { 1, 2, 3 }, 20).ToHashSet());
+
+    using (StreamWriter sr = new StreamWriter(pathPresentationFig + "GW_example.obj")) {
+      PrVsObj(sr, cube);
+      PrVsObj(sr, ellips);
+    }
+    HashSet<Vector> S    = ellips.Vertices.Union(cube.Vertices).ToHashSet();
+    ConvexPolytop   conv = ConvexPolytop.AsVPolytop(S, true);
+    conv.WriteTXT_3D(pathPresentationFig + "GW_example_conv");
+  }
+
+  public static void PrVsObj(StreamWriter sr, ConvexPolytop P) {
+    foreach (Vector v in P.Vertices) {
+      sr.WriteLine($"v {v.ToStrSepBySpace()}");
+    }
   }
 
 }
