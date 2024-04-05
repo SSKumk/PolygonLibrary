1e77ccb133caa90415f82a16b62f5740cfd31428
Anton Mikhailov <tigr-mav@yandex.ru>
2024-04-05 14:29:02 +0500

    Update GWBench_Cycles.cs

--- PolygonLibrary/Profile/Benchmarks/GWBench_Cycles.cs
+++ PolygonLibrary/Profile/Benchmarks/GWBench_Cycles.cs
@@ -22,8 +22,7 @@ public class GWBench_Cycles
 
   private ConvexPolytop? cycle;
 
-  // [Params(8, 16, 32, 64, 128)]
-  [Params(8, 16, 32, 64, 128)]
+  [Params(8, 16, 32)]
   // ReSharper disable once UnassignedField.Global
   public int amountPointsOnMomentCurve;
 
@@ -31,26 +30,41 @@ public class GWBench_Cycles
   public void SetUp()
   {
     cycle = ConvexPolytop.Cyclic
-      (dim, amountPointsOnMomentCurve, 1.0 / (amountPointsOnMomentCurve));
+      (dim, amountPointsOnMomentCurve, 1);
   }
 
-  /*
-    Заворачиваем циклические многогранники. Количество точек было выбрано "из головы".
-
-  */
   [Benchmark]
   public void GWCyclicPolytop() => GiftWrapping.WrapVRep(cycle!.VRep);
 
 
-  // public class Program
-  // {
-  //
-  //   public static void Main(string[] args)
-  //   {
-  //     var summary = BenchmarkRunner.Run<GWBench_Cycles>(
-  //       DefaultConfig.Instance.WithSummaryStyle(
-  //         SummaryStyle.Default.WithTimeUnit(TimeUnit.Millisecond)));
-  //   }
-  //
-  // }
+  public class Program
+  {
+
+    public static void Main(string[] args)
+    {
+      var summary = BenchmarkRunner.Run<GWBench_Cycles>(
+        DefaultConfig.Instance.WithSummaryStyle(
+          SummaryStyle.Default.WithTimeUnit(TimeUnit.Second)));
+    }
+
+  }
 }
+
+/*
+  Заворачиваем циклические многогранники. Количество точек было выбрано "из головы".
+| Method          | dim | amountPointsOnMomentCurve | Mean      | Error    | StdDev   |
+|---------------- |---- |-------------------------- |----------:|---------:|---------:|
+| GWCyclicPolytop | 3   | 8                         |  0.0005 s | 0.0000 s | 0.0000 s |
+| GWCyclicPolytop | 3   | 16                        |  0.0016 s | 0.0002 s | 0.0000 s |
+| GWCyclicPolytop | 3   | 32                        |  0.0048 s | 0.0004 s | 0.0000 s |
+| GWCyclicPolytop | 4   | 8                         |  0.0040 s | 0.0012 s | 0.0001 s |
+| GWCyclicPolytop | 4   | 16                        |  0.0237 s | 0.0085 s | 0.0005 s |
+| GWCyclicPolytop | 4   | 32                        |  0.1298 s | 0.2208 s | 0.0121 s |
+| GWCyclicPolytop | 5   | 8                         |  0.0183 s | 0.0004 s | 0.0000 s |
+| GWCyclicPolytop | 5   | 16                        |  0.1488 s | 0.0053 s | 0.0003 s |
+| GWCyclicPolytop | 5   | 32                        |  0.8125 s | 0.0104 s | 0.0006 s |
+| GWCyclicPolytop | 6   | 8                         |  0.0799 s | 0.0036 s | 0.0002 s |
+| GWCyclicPolytop | 6   | 16                        |  1.8076 s | 0.1091 s | 0.0060 s |
+| GWCyclicPolytop | 6   | 32                        | 21.0714 s | 8.2077 s | 0.4499 s |
+
+*/
