45c9769c793d7af456daadeb968ec76d7a9147c3
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-04-05 12:59:51 +0500

    Update GWBench_Spheres.cs

--- PolygonLibrary/Profile/Benchmarks/GWBench_Spheres.cs
+++ PolygonLibrary/Profile/Benchmarks/GWBench_Spheres.cs
@@ -13,8 +13,7 @@ using static Geometry<ddouble, Tests.DDConvertor>;
 using static Tests.ToolsTests.TestsPolytopes<ddouble, Tests.DDConvertor>;
 
 [ShortRunJob]
-// [WarmupCount(1)]
-// [IterationCount(1)]
+[WarmupCount(1)]
 public class GWBenchSpheres {
 
   // [Params(3, 4, 5, 6)]
@@ -24,11 +23,11 @@ public class GWBenchSpheres {
 
   private ConvexPolytop? polytop;
 
-  // [Params(0,10,100,1000)]
-  // // ReSharper disable once UnassignedField.Global
-  // public int amount;
+  [Params(0, 10, 100, 1000)]
+  // ReSharper disable once UnassignedField.Global
+  public int amount;
 
-  [Params( 10, 18)]
+  [Params(10, 18)]
   // ReSharper disable once UnassignedField.Global
   public int thetaPartition;
 
@@ -39,26 +38,47 @@ public class GWBenchSpheres {
   [GlobalSetup]
   public void SetUp() {
     var sphere = Sphere_list(dim, thetaPartition, phiPartition, 3);
-    polytop = ConvexPolytop.AsVPolytop(sphere.ToHashSet());
+    var S      = sphere.Union(SimplexRND(dim, out _, new int[] { dim }, amount - dim - 1));
+    polytop = ConvexPolytop.AsVPolytop(S.ToHashSet());
   }
 
 /*
 Заворачиваем сферы с дополнительными точками
+| Method   | dim | amount | thetaPartition | phiPartition | Mean      | Error     | StdDev   |
+|--------- |---- |------- |--------------- |------------- |----------:|----------:|---------:|
+| GWSphere | 3   | 0      | 10             | 10           |  0.0164 s |  0.0014 s | 0.0001 s |
+| GWSphere | 3   | 0      | 10             | 16           |  0.0359 s |  0.0088 s | 0.0005 s |
+| GWSphere | 3   | 0      | 18             | 10           |  0.0449 s |  0.0053 s | 0.0003 s |
+| GWSphere | 3   | 0      | 18             | 16           |  0.1027 s |  0.0045 s | 0.0002 s |
+| GWSphere | 3   | 10     | 10             | 10           |  0.0170 s |  0.0019 s | 0.0001 s |
+| GWSphere | 3   | 10     | 10             | 16           |  0.0363 s |  0.0036 s | 0.0002 s |
+| GWSphere | 3   | 10     | 18             | 10           |  0.0462 s |  0.0085 s | 0.0005 s |
+| GWSphere | 3   | 10     | 18             | 16           |  0.1038 s |  0.0251 s | 0.0014 s |
+| GWSphere | 3   | 100    | 10             | 10           |  0.0293 s |  0.0017 s | 0.0001 s |
+| GWSphere | 3   | 100    | 10             | 16           |  0.0555 s |  0.0034 s | 0.0002 s |
+| GWSphere | 3   | 100    | 18             | 10           |  0.0666 s |  0.0055 s | 0.0003 s |
+| GWSphere | 3   | 100    | 18             | 16           |  0.1317 s |  0.0144 s | 0.0008 s |
+| GWSphere | 3   | 1000   | 10             | 10           |  0.1502 s |  0.0054 s | 0.0003 s |
+| GWSphere | 3   | 1000   | 10             | 16           |  0.2356 s |  0.0633 s | 0.0035 s |
+| GWSphere | 3   | 1000   | 18             | 10           |  0.2645 s |  0.0183 s | 0.0010 s |
+| GWSphere | 3   | 1000   | 18             | 16           |  0.4290 s |  0.0612 s | 0.0034 s |
+| GWSphere | 4   | 0      | 10             | 10           |  2.0000 s |  0.5791 s | 0.0317 s |
+| GWSphere | 4   | 0      | 10             | 16           |  4.0037 s |  1.5619 s | 0.0856 s |
+| GWSphere | 4   | 0      | 18             | 10           | 14.9503 s |  2.3061 s | 0.1264 s |
+| GWSphere | 4   | 0      | 18             | 16           | 35.6670 s |  3.4093 s | 0.1869 s |
+| GWSphere | 4   | 10     | 10             | 10           |  1.9472 s |  0.1312 s | 0.0072 s |
+| GWSphere | 4   | 10     | 10             | 16           |  3.9371 s |  0.5301 s | 0.0291 s |
+| GWSphere | 4   | 10     | 18             | 10           | 15.0854 s |  1.0379 s | 0.0569 s |
+| GWSphere | 4   | 10     | 18             | 16           | 36.4134 s |  3.9148 s | 0.2146 s |
+| GWSphere | 4   | 100    | 10             | 10           |  2.5002 s |  3.5168 s | 0.1928 s |
+| GWSphere | 4   | 100    | 10             | 16           |  4.3221 s |  5.2738 s | 0.2891 s |
+| GWSphere | 4   | 100    | 18             | 10           | 16.4536 s | 16.7765 s | 0.9196 s |
+| GWSphere | 4   | 100    | 18             | 16           | 36.1814 s | 16.3318 s | 0.8952 s |
+| GWSphere | 4   | 1000   | 10             | 10           |  4.0765 s |  3.2863 s | 0.1801 s |
+| GWSphere | 4   | 1000   | 10             | 16           |  6.5171 s |  0.5906 s | 0.0324 s |
+| GWSphere | 4   | 1000   | 18             | 10           | 20.7711 s | 16.2601 s | 0.8913 s |
+| GWSphere | 4   | 1000   | 18             | 16           | 44.4061 s | 47.0076 s | 2.5766 s |
 
-| Method   | dim | thetaPartition | phiPartition | Mean       | Error |
-|--------- |---- |--------------- |------------- |-----------:|------:|
-| GWSphere | 3   | 10             | 10           |   0.0222 s |    NA |
-| GWSphere | 3   | 10             | 16           |   0.0526 s |    NA |
-| GWSphere | 3   | 18             | 10           |   0.0671 s |    NA |
-| GWSphere | 3   | 18             | 16           |   0.1601 s |    NA |
-| GWSphere | 3   | 36             | 10           |   0.2446 s |    NA |
-| GWSphere | 3   | 36             | 16           |   0.6793 s |    NA |
-| GWSphere | 4   | 10             | 10           |   2.5352 s |    NA |
-| GWSphere | 4   | 10             | 16           |   6.1830 s |    NA |
-| GWSphere | 4   | 18             | 10           |  26.8099 s |    NA |
-| GWSphere | 4   | 18             | 16           |  57.5789 s |    NA |
-| GWSphere | 4   | 36             | 10           | 361.7732 s |    NA |
-| GWSphere | 4   | 36             | 16           | 924.0218 s |    NA |
 
  */
 
@@ -66,13 +86,13 @@ public class GWBenchSpheres {
   public void GWSphere() => GiftWrapping.WrapVRep(polytop!.VRep);
 
 
-  public class Program {
-
-    public static void Main(string[] args) {
-      var summary = BenchmarkRunner.Run<GWBenchSpheres>
-        (DefaultConfig.Instance.WithSummaryStyle(SummaryStyle.Default.WithTimeUnit(TimeUnit.Second)));
-    }
-
-  }
+  // public class Program {
+  //
+  //   public static void Main(string[] args) {
+  //     var summary = BenchmarkRunner.Run<GWBenchSpheres>
+  //       (DefaultConfig.Instance.WithSummaryStyle(SummaryStyle.Default.WithTimeUnit(TimeUnit.Second)));
+  //   }
+  //
+  // }
 
 }
