38befd0bf0fc66f698dafcf7cc2356e0c3c7a21a
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-04-26 18:58:56 +0500

    +

--- PolygonLibrary/Profile/Profile.cs
+++ PolygonLibrary/Profile/Profile.cs
@@ -17,17 +17,49 @@ class Program {
   private static readonly string pathData =
     "F:/Works/IMM/Аспирантура/_PolygonLibrary/PolygonLibrary/Tests/OtherTests/LDG_computations/";
 
+
+
   static void Main(string[] args) {
     CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+    ConvexPolytop A = ConvexPolytop.SimplexRND(6);
+    ConvexPolytop B = ConvexPolytop.SimplexRND(6);
+    ConvexPolytop sum = MinkowskiSum.BySandipDas(A, B);
+    ConvexPolytop sumCH = MinkowskiSum.ByConvexHull(A, B);
+    Console.WriteLine(string.Join(' ', A.FVector));
+    Console.WriteLine(string.Join(' ', B.FVector));
+    Console.WriteLine(string.Join(' ', sum.FVector));
+    Console.WriteLine(string.Join(' ', sumCH.FVector));
+    // ConvexPolytop sumCutted = MinkowskiSum.BySandipDasCutted(A, B);
+    // Console.WriteLine(sum.Vertices.SetEquals(sumCutted.Vertices));
 
-    // SolverLDG solverLdg = new SolverLDG(pathData, "materialDot1-1-supG");
-    SolverLDG solverLdg = new SolverLDG(pathData, "materialDot1-0.9-supG");
-    // SolverLDG solverLdg = new SolverLDG(pathData, "oscillator1-1-supG");
-    // SolverLDG solverLdg = new SolverLDG(pathData, "Spheres3D");
+
+    //
+    // ConvexPolytop P = ConvexPolytop.Cube01(2);
+    // ConvexPolytop Q = ConvexPolytop.AsFLPolytop(new List<Vector>()
+    //   {
+    //     Vector.Zero(2),
+    //     Vector.Ones(2)
+    //   });
+    //
+    // ConvexPolytop sum = MinkowskiSum.BySandipDas(P, Q);
+
+    // for (int i = 3; i <= 7; i++) {
+    //   ConvexPolytop P = ConvexPolytop.Cube01(i).RotateRND(true);
+    //   ConvexPolytop Q = ConvexPolytop.Cube01(i).RotateRND(true);
+    //   Console.WriteLine($"{i} | {string.Join(' ', MinkowskiSum.BySandipDas(P, Q).FVector)}");
+    //   Console.Out.Flush();
+    // }
+
+    // SolverLDG.WriteSimplestTask_TerminalSet_GameItself(5, pathData);
+
+    // SolverLDG solverLdg = new SolverLDG(pathData, "materialDot1-0.9-supG");
     // SolverLDG solverLdg = new SolverLDG(pathData, "oscillator1-0.9-supG");
+    // SolverLDG solverLdg = new SolverLDG(pathData, "simpleMotion");
+    // SolverLDG solverLdg = new SolverLDG(pathData, "simplestGame_5D");
 
+    // solverLdg.Solve(true);
 
-    solverLdg.Solve(true);
   }
+
 
 }
--- PolygonLibrary/Tests/OtherTests/Sandbox.cs
+++ PolygonLibrary/Tests/OtherTests/Sandbox.cs
@@ -6,10 +6,8 @@ using static Tests.ToolsTests.TestsPolytopes<DoubleDouble.ddouble, Tests.DDConve
 using static Tests.ToolsTests.TestsBase<DoubleDouble.ddouble, Tests.DDConvertor>;
 using System.IO;
 using DoubleDouble;
-using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
-
-
-// using static CGLibrary.Geometry<double, Tests.DConvertor>;
+// using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
+using static CGLibrary.Geometry<double, Tests.DConvertor>;
 
 
 namespace Tests.OtherTests;
@@ -25,16 +23,65 @@ public class Sandbox {
 
   [Test]
   public void Sandboxx() {
+    // List<HyperPlane> hp = new List<HyperPlane>()
+    //   {
+    //     new HyperPlane(new Vector(new double[] { -1, 0, 0, 0 }), 0)
+    //   , new HyperPlane(new Vector(new double[] { 0, -1, 0, 0 }), 0)
+    //   , new HyperPlane(new Vector(new double[] { 0, 0, -1, 0 }), 0)
+    //   , new HyperPlane(new Vector(new double[] { 0, 0, 0, -1 }), 0)
+    //   , new HyperPlane(new Vector(new double[] { 1, 1, 1, 0 }), 1)
+    //   , new HyperPlane(new Vector(new double[] { -1, -1, -1, 0 }), -1)
+    //   , new HyperPlane(new Vector(new double[] { -1, -1, 0, 1 }), -0.5)
+    //   , new HyperPlane(new Vector(new double[] { 1, 1, 0, -1 }), 0.5)
+    //   };
+
+    List<HyperPlane> hp = new List<HyperPlane>()
+      {
+        new HyperPlane(new Vector(new double[] { -1, 0, 0, 0, 0 }), 0)
+      , new HyperPlane(new Vector(new double[] { 0, -1, 0, 0, 0 }), 0)
+      , new HyperPlane(new Vector(new double[] { 0, 0, -1, 0, 0 }), 0)
+      , new HyperPlane(new Vector(new double[] { 0, 0, 0, -1, 0 }), 0)
+      , new HyperPlane(new Vector(new double[] { 0, 0, 0, 0, -1 }), 0)
+      , new HyperPlane(new Vector(new double[] { 1, 1, 1, 0, 0 }), 3)
+      , new HyperPlane(new Vector(new double[] { -1, -1, -1, 0, 0 }), -3)
+      , new HyperPlane(new Vector(new double[] { -2, -1, 0, 1, 0 }), -2)
+      , new HyperPlane(new Vector(new double[] { 2, 1, 0, -1, 0 }), 2)
+      , new HyperPlane(new Vector(new double[] { -1, -2, 0, 0, 1 }), -2)
+      , new HyperPlane(new Vector(new double[] { 1, 2, 0, 0, -1 }), 2)
+      };
+    var x = ConvexPolytop.HRepToVRep_Naive(hp);
+    Console.WriteLine(string.Join('\n', x));
+
+    // ConvexPolytop Q = ConvexPolytop.Cyclic(5, 12, 1.457);
+    // ConvexPolytop? diff = MinkowskiDiff.Naive(P, Q);
+    // Console.WriteLine($"diff is null = {diff is null}");
+
+    // ConvexPolytop trapz = ConvexPolytop.AsHPolytop(new List<Vector>()
+    //   {
+    //     new Vector(new ddouble[]{0,2}),
+    //     new Vector(new ddouble[]{0,4}),
+    //     new Vector(new ddouble[]{1,4}),
+    //     new Vector(new ddouble[]{3,2}),
+    //   });
+    // ConvexPolytop triag = ConvexPolytop.AsHPolytop(new List<Vector>()
+    //   {
+    //     new Vector(new ddouble[]{0,0.6}),
+    //     new Vector(new ddouble[]{1,1}),
+    //     new Vector(new ddouble[]{2,0}),
+    //   });
+    //
+    // ConvexPolytop? diff = MinkowskiDiff.Naive(trapz, triag);
 
     // for (int i = 3; i <= 7; i++) {
-    //   ConvexPolytop P = ConvexPolytop.SimplexRND(i,true);
-    //   ConvexPolytop Q = ConvexPolytop.SimplexRND(i,true);
+    //   ConvexPolytop P = ConvexPolytop.Cube01(i).RotateRND(true);
+    //   ConvexPolytop Q = ConvexPolytop.Cube01(i).RotateRND(true);
     //   Console.WriteLine($"{i} | {string.Join(' ', MinkowskiSum.BySandipDas(P,Q).FVector)}");
+    //   Console.Out.Flush();
     // }
-    Console.WriteLine($"6 | {string.Join(' ', ConvexPolytop.Cyclic(6,64,0.01).FVector)}");
+    // Console.WriteLine($"6 | {string.Join(' ', ConvexPolytop.Cyclic(6,64,0.01).FVector)}");
 
     // ConvexPolytop ellips = ConvexPolytop.Ellipsoid(3, 20, 100, Vector.Zero(3), new Vector(new ddouble[] { 1, 2, 3 }));
-    // ConvexPolytop cube   = ConvexPolytop.AsVPolytop(Cube(3, out _, new int[] { 1, 2, 3 }, 20).ToHashSet());
+    // ConvexPolytop cube   = ConvexPolytop.AsVPolytop(Cube01(3, out _, new int[] { 1, 2, 3 }, 20).ToHashSet());
 
     // ConvexPolytop cube  = ConvexPolytop.RectParallel(-Vector.Ones(3), Vector.Ones(3));
     // ConvexPolytop candy = ConvexPolytop.Sphere(3, 2, 4, Vector.Zero(3), 1);
@@ -45,7 +92,7 @@ public class Sandbox {
     // ConvexPolytop sum = MinkowskiSum.BySandipDas(cube, candy);
     // sum.WriteTXT_3D(pathPresentationFig + "Cube01_plus_Octahedron");
     // using (StreamWriter sr = new StreamWriter(pathPresentationFig + "GeneralPos.obj")) {
-      // PrVsObj(sr, ConvexPolytop.Cube01(3));
+    // PrVsObj(sr, ConvexPolytop.Cube01(3));
     // }
     // conv.WriteTXT_3D(pathPresentationFig + "GW_example_conv");
   }
