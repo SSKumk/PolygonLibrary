7c35eccfd5f5a9e2ad79c0874ec288a0de0cb7a1
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-07-16 19:18:53 +0500

    Update Profile.cs

--- PolygonLibrary/Profile/Profile.cs
+++ PolygonLibrary/Profile/Profile.cs
@@ -24,141 +24,103 @@ namespace Profile;
 
 using System;
 
-class Program
-{
+class Program {
 
   private static readonly string pathData =
     "F:/Works/IMM/Аспирантура/_PolygonLibrary/PolygonLibrary/Tests/OtherTests/LDG_computations/";
 
 
-  static void Main(string[] args)
-  {
+  static void Main(string[] args) {
     CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
-    // LinearBasis lb = new LinearBasis(3,5);
-    // lb.AddVector(Vector.GenVector(5));
-    // System.Console.WriteLine(lb[3]);
-    // System.Console.WriteLine(lb[3].Length);
-    // LinearBasis.CheckCorrectness(lb);
 
-    // Matrix A = new Matrix(new double[,] { { 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 1 }, { 1, 0, 0, 0, 0 } });
-    // A = A.Transpose();
+    Tools.Eps = 1e-8;
 
-    // Matrix A = Matrix.GenMatrix(3,3,-10,10,new GRandomLC(10));
-    // (var Q, var R) = QRDecomposition.ByReflection(A);
-    // Console.WriteLine($"A:\n{A}");
-    // Console.WriteLine($"Q:\n{Q}");
-    // Console.WriteLine($"R:\n{R}");
-    // Console.WriteLine($"Q*R:\n{Q * R}");
-    // Console.WriteLine((Q * R - A).Equals(Matrix.Zero(A.Rows, A.Cols)));
-    // Console.WriteLine(Q.TakeVector(0) * Q.TakeVector(2));
+    // Vector v1 = new Vector(new double[] { -1.0596541049778718, 1.1515585830401667, 2 });
+    // Vector v2 = new Vector(new double[] { -1.0596541050169939, 1.151558582963422, 2 });
+
+    Vector v1 = new Vector(new double[] { 1.9100000000000001, 0.09999999999999969, 2 });
+    Vector v2 = new Vector(new double[] { 1.9100000000000001, 0.10000000000000739, 2 });
+
+    // Vector v1 = new Vector(new double[] { -1.554574501452087, 0.4747626291950904, 2 });
+    // Vector v2 = new Vector(new double[] { -1.5545745015129653, 0.4747626289226897, 2 }); // 11.0  11.1  11.2  11.3  11.4  11.5
+
+    Console.WriteLine(v1 == v2);
+
+    Console.WriteLine($"{v1[0]}: {Tools.GetHashCodeTNum(v1[0])}");
+    Console.WriteLine($"{v2[0]}: {Tools.GetHashCodeTNum(v2[0])}");
+    Console.WriteLine($"{v1[1]}: {Tools.GetHashCodeTNum(v1[1])}");
+    Console.WriteLine($"{v2[1]}: {Tools.GetHashCodeTNum(v2[1])}");
+    Console.WriteLine($"{v2[1]}: {Tools.GetHashCodeTNum(v2[1] + Tools.Eps)}");
+
+    // Console.WriteLine(Math.Floor(2.5));
+    // Console.WriteLine(Math.Floor(-2.5));
+
+    // Console.WriteLine(Tools.GetHashCodeTNum(-0.09999999999999969));
+    // Console.WriteLine(Tools.GetHashCodeTNum(-0.10000000000000739));
+    // Console.WriteLine(Tools.EQ(-0.09999999999999969, -0.10000000000000739));
+
+    string materialdot_path =
+      pathData + "Ep_MaterialDot-1-0.9_T[4,7]_P#RectParallel_Q#RectParallel_M#DtnOrigin_Ball_2-T4-P100_-CMax2/";
 
 
-    // System.Console.WriteLine(LinearBasis.IsContains(Vector.Ones(3).LiftUp(5, 0), new LinearBasis(5, 3)));
-
-
-
-    // AffineBasis affb  = new AffineBasis(new Vector[] { origin, B, C });
-    // HyperPlane  hpaff = new HyperPlane(affb);
-    // Console.WriteLine(hpaff.Normal * (B - origin));
-
-    // Matrix A = Matrix.hcat
-    //   (affb.LinBasis, new Matrix(affb.LinBasis.VecDim, 1, Vector.MakeOrth(3, 1).GetAsArray()));
-    // QRDecomposition q = new QRDecomposition(A);
+    // double t = 7.0;
+    // while (t > 5.9) {
+    //   Console.WriteLine($"t = {t:F1}");
+    //    ParamReader prFirst  = new ParamReader($"{materialdot_path}/double/Naive/1e-008/{t:F2})materialDot1-0.9-supG.cpolytop");
+    //   ParamReader prSecond = new ParamReader($"{materialdot_path}/double/Geometric/1e-008/{t:F2})materialDot1-0.9-supG.cpolytop");
     //
-    // Console.WriteLine(q.Q.TakeVector(2) * (B - origin));
+    //   ConvexPolytop first  = ConvexPolytop.AsFLPolytop(prFirst);
+    //   ConvexPolytop second = ConvexPolytop.AsFLPolytop(prSecond);
     //
-    // Console.WriteLine(q.R);
-
-
-    // Console.WriteLine(Vector.OrthonormalizeAgainstBasis(B - origin, affb.Basis));
-
-    // Console.WriteLine(lb.GetMatrix());
-
-    // Matrix A = new Matrix(new double[,] { { 1, 2.5307302532090645 }, { 0, 3.7737420279937193 }, { 0, -2.0523162773000814 } });
-    // QRDecomposition q = new QRDecomposition(A);
-    // Console.WriteLine(q.Q);
-
-
-    // Matrix          A = Matrix.Hilbert(15);
-    // LinearBasis linearBasis = LinearBasis.FromMatrix(A);
-
-    // Console.WriteLine(q.Q);
-    // Console.WriteLine();
-    // Console.WriteLine(linearBasis.GetMatrix());
-
-    // Console.WriteLine(q.Q.TakeVector(0) + linearBasis.Basis[0]);
-
-
-    // Console.WriteLine((q.Q * q.R).Equals(A));
-
-    // string      matDot = "Ep_MaterialDot-1-0.9_T[4,7]_P#RectParallel_Q#RectParallel_M#DtnOrigin_Ball_2-T4-P100_-CMax2";
+    //   Console.WriteLine(first.VRep.SetEquals(second.VRep));
+    //   List<Vector> lnaive     = new List<Vector>(first.VRep).Order().ToList();
+    //   List<Vector> lgeometric = new List<Vector>(second.VRep).Order().ToList();
     //
-    // string      t      = "4.60)";
-    // ParamReader prSec     = new ParamReader($"{pathData}{matDot}/{t}materialDot1-0.9-supG.tsection");
-    // ParamReader prP     = new ParamReader($"{pathData}{matDot}/{t} P materialDot1-0.9-supG.tsection");
-    // ParamReader prQ     = new ParamReader($"{pathData}{matDot}/{t} Q materialDot1-0.9-supG.tsection");
-    // var x1 = prSec.ReadNumber<ddouble>("t");
-    // var x2 = prP.ReadNumber<ddouble>("t");
-    // var x3 = prQ.ReadNumber<ddouble>("t");
-
-    // ConvexPolytop sec = ConvexPolytop.AsFLPolytop(prSec);
-    // ConvexPolytop P   = ConvexPolytop.AsFLPolytop(prP);
-    // ConvexPolytop Q   = ConvexPolytop.AsFLPolytop(prQ);
-
-    // Console.WriteLine(sec.MinDistBtwVs());
-
-
-    // ConvexPolytop A = ConvexPolytop.AsFLPolytop(pr);
-    // Console.WriteLine(A.VRep.Count);
+    //   List<int> lnaive_Hash     = new List<Vector>(first.VRep).Order().Select(v => v.GetHashCode()).ToList();
+    //   List<int> lgeometric_Hash = new List<Vector>(second.VRep).Order().Select(v => v.GetHashCode()).ToList();
     //
-    // pr = new ParamReader($"{pathData}{matDot}/{t} P materialDot1-0.9-supG.tsection");
-    // pr.ReadNumber<ddouble>("t");
-    // ConvexPolytop P = ConvexPolytop.AsFLPolytop(pr);
-    // Console.WriteLine(P.VRep.Count);
+    //   int    diff  = 0;
+    //   double error = 0;
+    //   for (int i = 0; i < lnaive.Count; i++) {
+    //     if (lnaive[i] != lgeometric[i]) {
+    //       Console.WriteLine("el is diff");
+    //       Console.WriteLine($"{lnaive[i]}");
+    //       Console.WriteLine($"{lgeometric[i]}");
+    //       Console.WriteLine();
     //
-    // pr = new ParamReader($"{pathData}{matDot}/{t} Q materialDot1-0.9-supG.tsection");
-    // pr.ReadNumber<ddouble>("t");
-    // ConvexPolytop Q = ConvexPolytop.AsVPolytop(pr);
-    // Console.WriteLine(Q.VRep.Count);
+    //       diff++;
+    //     }
+    //     // else {
+    //       // Vector vector = lnaive[i] - lgeometric[i];
+    //       // error += vector.GetAsArray().Sum(double.Abs) / vector.Length;
+    //     // }
     //
-    // ConvexPolytop  x = MinkowskiSum.BySandipDas(A, Q);
-    // ConvexPolytop? y = MinkowskiDiff.H2VGeometric(x, Q);
+    //     if (lnaive_Hash[i] != lgeometric_Hash[i]) {
+    //       Console.WriteLine("hash is diff");
+    //       Console.WriteLine($"{lnaive[i]}");
+    //       Console.WriteLine($"{lgeometric[i]}");
+    //       Console.WriteLine();
+    //     }
+    //   }
+    //   // Console.WriteLine(error / lnaive.Count);
+    //   Console.WriteLine(diff);
     //
-    // Console.WriteLine(y!.VRep.Count);
+    //   // break;
+    //
+    //   if (!first.Equals(second)) {
+    //     throw new ArgumentException($"NOT equal at t = {t}");
+    //   }
+    //
+    //   t -= 0.1;
+    // }
+    //
 
 
-    // ConvexPolytop A       = ConvexPolytop.SimplexRND(4).RotateRND();
-    // ConvexPolytop B       = ConvexPolytop.SimplexRND(4).RotateRND();
-    // ConvexPolytop pp      = MinkowskiSum.BySandipDas(A, B);
-    // ConvexPolytop ppc      = MinkowskiSum.BySandipDas(A, B, true);
-    // Console.WriteLine(pp.HRep.Count);
-    // Console.WriteLine(ppc.HRep.Count);
+    // SolverLDG solverLdg = new SolverLDG(pathData, "materialDot1-0.9-supG");
 
-    // ConvexPolytop polytop = ConvexPolytop.AsVPolytop(ConvexPolytop.Cube01(4).VRep.Union(new List<Vector>()
-    //   {
-    //     new Vector(new ddouble[]{0.5,0.5,0.5,2}), // - в общем-то, это не нужно, чтобы сломать
-    //     new Vector(new ddouble[]{0.5,0.5,0.5,-2})
-    //   }));
-
-
-    // var           g       = ConvexPolytop.HRepToVRep_Geometric(polytop.HRep);
-    // Console.WriteLine("polytop == geometricH2V");
-    // Console.WriteLine(polytop.VRep.SetEquals(g));
-    // Console.WriteLine("polytop \\subset geometricH2V");
-    // Console.WriteLine(polytop.VRep.IsSubsetOf(g));
-    // Console.WriteLine("geometricH2V \\ polytop");
-    // Console.WriteLine(string.Join('\n', g.Except(polytop.VRep)));
-    // Console.WriteLine("polytop \\ geometricH2V");
-    // Console.WriteLine(string.Join('\n', polytop.VRep.Except(g)));
-
-
-    // SolverLDG.WriteSimplestTask_TerminalSet_GameItself(5, pathData);
-
-    SolverLDG solverLdg = new SolverLDG(pathData, "materialDot1-0.9-supG");
-    // SolverLDG solverLdg = new SolverLDG(pathData, "oscillator1-0.9-supG");
-
-    solverLdg.Solve(false);
+    // solverLdg.Solve(false, true, false);
+    // solverLdg.Solve(true, true, false);
   }
 
 }
