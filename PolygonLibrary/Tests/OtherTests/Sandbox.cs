f4f02ce62a5ed775d66a6a1e48f304fe6f3e6124
Anton Mikhailov <tigr-mav@yandex.ru>
2024-10-07 18:29:44 +0500

    Update Sandbox.cs

--- PolygonLibrary/Tests/OtherTests/Sandbox.cs
+++ PolygonLibrary/Tests/OtherTests/Sandbox.cs
@@ -6,8 +6,8 @@ using static Tests.ToolsTests.TestsPolytopes<DoubleDouble.ddouble, Tests.DDConve
 using static Tests.ToolsTests.TestsBase<DoubleDouble.ddouble, Tests.DDConvertor>;
 using System.IO;
 using DoubleDouble;
-// using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
-using static CGLibrary.Geometry<double, Tests.DConvertor>;
+using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
+// using static CGLibrary.Geometry<double, Tests.DConvertor>;
 
 
 namespace Tests.OtherTests;
@@ -20,8 +20,20 @@ public class Sandbox {
 
   [Test]
   public void Sandboxx() {
-    var x = pathData;
+    int dim = 5;
+    var x   = Cube01(dim, out List<Vector> cube);
+    var y   = SimplexRND(dim, out _);
+    ShiftAndRotate(dim, ref cube, ref x);
 
+    var xP = ConvexPolytop.CreateFromPoints(x);
+    var yP = ConvexPolytop.CreateFromPoints(y);
+    var xy = MinkowskiSum.BySandipDas(xP, yP);
+    
+    var h  = xy.Hrep;
+
+    var hG = ConvexPolytop.CreateFromPoints(ConvexPolytop.HrepToVrep_Geometric(h));
+
+    Console.WriteLine($"{xy.Vrep.SetEquals(hG.Vrep)}");
   }
 
 
