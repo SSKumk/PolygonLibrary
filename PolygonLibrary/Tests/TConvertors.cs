c7e10778004c4e994813837b58a53a4d430c42cf
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-11-12 18:08:02 +0500

    Update TConvertors.cs

--- PolygonLibrary/Tests/TConvertors.cs
+++ PolygonLibrary/Tests/TConvertors.cs
@@ -1,6 +1,5 @@
 using CGLibrary;
 using DoubleDouble;
-using MultiPrecision;
 
 namespace Tests;
 //ТЕСТЫ очередного типа TNum: vvvv
@@ -34,26 +33,5 @@ public class DDConvertor : INumConvertor<ddouble> {
   public static ddouble FromInt(int       from) => from;
   public static uint    ToUInt(ddouble    from) => (uint)from;
   public static ddouble FromUInt(uint     from) => from;
-
-}
-
-public struct N8 : IConstant {
-
-  public readonly int Value => 8;
-
-}
-
-
-/// <summary>
-/// Interface for multi-precision conversions.
-/// </summary>
-public class MPConvertor : INumConvertor<MultiPrecision<N8>> {
-
-  public static double             ToDouble(MultiPrecision<N8> from) => (double)from;
-  public static MultiPrecision<N8> FromDouble(double           from) => from;
-  public static int                ToInt(MultiPrecision<N8>    from) => (int)from;
-  public static MultiPrecision<N8> FromInt(int                 from) => from;
-  public static uint               ToUInt(MultiPrecision<N8>   from) => (uint)from;
-  public static MultiPrecision<N8> FromUInt(uint               from) => from;
 
 }
