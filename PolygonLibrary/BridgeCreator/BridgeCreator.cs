87d570152e30983efe2a04c67c9384891d6c1782
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2024-11-19 18:39:44 +0500

    rename BridgeCreator

---
+++ PolygonLibrary/BridgeCreator/BridgeCreator.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Numerics;
+using CGLibrary;
+
+
+namespace BridgeCreator;
+// todo:  1. Счёт мостов. Файл настроек Игры, файл настроек набора Мостов
+//       (задано одно терминальное множество, задана функция платы и набор С, надграфик выпуклой функции)
+
+class BridgeCreator<TNum, TConv>
+  where TNum : struct, INumber<TNum>, ITrigonometricFunctions<TNum>, IPowerFunctions<TNum>, IRootFunctions<TNum>,
+  IFloatingPoint<TNum>, IFormattable
+  where TConv : INumConvertor<TNum> {
+
+  public Geometry<TNum, TConv>.SolverLDG sl;
+
+  public BridgeCreator(string workDir, string taskDir, string taskName, string bridgeConfigDir) {
+
+  }
+
+}
+
+
+class Program {
+
+  static void Main(string[] args) {
+    var x = new BridgeCreator<double, DConvertor>();
+  }
+
+}
