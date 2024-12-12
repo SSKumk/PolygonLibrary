fdc987dd376f4d807e08e67783a6126339c48a9e
Anton Mikhailov <tigr-mav@yandex.ru>
2024-12-12 17:56:23 +0500

    Epigraph reader

---
+++ PolygonLibrary/Bridges/TerminalSet/EpigraphType/EpiTypes.cs
@@ -0,0 +1,25 @@
+﻿namespace Bridges.EpigraphType;
+
+public abstract class EpiTypes<TNum, TConv>
+  where TNum : struct, INumber<TNum>, ITrigonometricFunctions<TNum>, IPowerFunctions<TNum>, IRootFunctions<TNum>,
+  IFloatingPoint<TNum>, IFormattable
+  where TConv : INumConvertor<TNum> {
+
+  public class DistToPoint : IEpiType<TNum, TConv> {
+    public Geometry<TNum, TConv>.Vector Point { get; private set; } = null!;
+
+    public void ReadParameters(Geometry<TNum, TConv>.ParamReader pr, LDGPathHolder<TNum, TConv> dh) {
+      Point = pr.ReadVector("Point");
+    }
+  }
+
+  public class DistToPolytope : IEpiType<TNum, TConv> {
+    public Geometry<TNum, TConv>.ConvexPolytop Polytope { get; private set; } = null!;
+
+
+    public void ReadParameters(Geometry<TNum, TConv>.ParamReader pr, LDGPathHolder<TNum, TConv> dh) {
+      Polytope = ITerminalSetReader<TNum, TConv>.DoPolytope(pr.ReadString("Name"), dh);
+    }
+  }
+
+}
