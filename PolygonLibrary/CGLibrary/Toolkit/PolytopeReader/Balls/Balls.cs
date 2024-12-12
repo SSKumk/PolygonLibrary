254bbeab669c5ee26e11619060fe63f1fed6ca35
Anton Mikhailov <tigr-mav@yandex.ru>
2024-12-12 17:56:06 +0500

    Ball reader

---
+++ PolygonLibrary/CGLibrary/Toolkit/PolytopeReader/Balls/Balls.cs
@@ -0,0 +1,25 @@
+﻿namespace CGLibrary;
+
+public partial class Geometry<TNum, TConv>
+  where TNum : struct, INumber<TNum>, ITrigonometricFunctions<TNum>, IPowerFunctions<TNum>, IRootFunctions<TNum>,
+  IFloatingPoint<TNum>, IFormattable
+  where TConv : INumConvertor<TNum> {
+  
+  public class Ball_1 : IBall {
+    public void ReadParameters(ParamReader pr) { }    // Ball_1 has no parameters
+  }
+
+  public class Ball_2 : IBall {
+    public int PolarDivision { get; private set; }
+    public int AzimuthsDivisions { get; private set; }
+
+    public void ReadParameters(ParamReader pr) {
+      PolarDivision    = pr.ReadNumber<int>("Polar");
+      AzimuthsDivisions = pr.ReadNumber<int>("Azimuths");
+    }
+  }
+
+  public class Ball_oo : IBall {
+    public void ReadParameters(ParamReader pr) { }  // Ball_oo has no parameters
+  }
+}
