a4ff14223660c7f8ddc2c3374804d0f9888a56c9
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-01-13 19:21:06 +0500

    Update IDrawer.cs

--- PolygonLibrary/Graphics/Draw/IDrawer.cs
+++ PolygonLibrary/Graphics/Draw/IDrawer.cs
@@ -1,10 +1,9 @@
-using CGLibrary;
 using static CGLibrary.Geometry<double, Graphics.DConvertor>;
 
 namespace Graphics.Draw;
 
 public interface IDrawer {
 
-  public void SaveFrame(Visualization.Facet Fs, List<Vector> Vs);
+  public void SaveFrame(string path, IEnumerable<Visualization.Facet> facets, IEnumerable<Vector> vertices);
 
 }
