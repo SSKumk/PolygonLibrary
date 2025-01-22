7ff86c20f9b7a352ada401230d5765cf4d7a345c
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-01-22 10:42:16 +0500

    Update Main.cs

--- PolygonLibrary/Bridges/Main.cs
+++ PolygonLibrary/Bridges/Main.cs
@@ -1,7 +1,7 @@
 using System.Globalization;
 using LDG;
 
-// using DoubleDouble;
+using DoubleDouble;
 // using static CGLibrary.Geometry<DoubleDouble.ddouble, Bridges.DDConvertor>;
 
 // using static CGLibrary.Geometry<double, Bridges.DConvertor>;
@@ -14,10 +14,11 @@ class Program {
     CGLibrary.Geometry<double, DConvertor>.Tools.Eps = 1e-8;
 
 
-    // string ldgDir = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
-    string ldgDir = "E:\\Work\\LDG\\_Out\\";
+    string ldgDir = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
+    // string ldgDir = "E:\\Work\\LDG\\_Out\\";
 
     BridgeCreator<double, DConvertor> bridgeCreator = new BridgeCreator<double, DConvertor>(ldgDir, "SimpleMotion.Test1");
+    // BridgeCreator<ddouble, DDConvertor> bridgeCreator = new BridgeCreator<ddouble, DDConvertor>(ldgDir, "Some3D");
     bridgeCreator.Solve();
   }
 
