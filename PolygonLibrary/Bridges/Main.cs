1b0c6469e30090f0d9eaf73d64bf63acb4de3b46
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-01-24 20:11:39 +0500

    Update Main.cs

--- PolygonLibrary/Bridges/Main.cs
+++ PolygonLibrary/Bridges/Main.cs
@@ -11,15 +11,13 @@ namespace Bridges;
 class Program {
 
   static void Main(string[] args) {
-    // CGLibrary.Geometry<double, DConvertor>.Tools.Eps   = 1e-8;
-    CGLibrary.Geometry<ddouble, DDConvertor>.Tools.Eps = 1e-16;
 
 
     string ldgDir = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
     // string ldgDir = "E:\\Work\\LDG\\_Out\\";
 
-    // BridgeCreator<double, DConvertor> bridgeCreator = new BridgeCreator<double, DConvertor>(ldgDir, "Oscillator");
-    BridgeCreator<ddouble, DDConvertor> bridgeCreator = new BridgeCreator<ddouble, DDConvertor>(ldgDir, "Oscillator");
+    // BridgeCreator<double, DConvertor> bridgeCreator = new BridgeCreator<double, DConvertor>(ldgDir, "Oscillator", 1e-08);
+    BridgeCreator<ddouble, DDConvertor> bridgeCreator = new BridgeCreator<ddouble, DDConvertor>(ldgDir, "Oscillator", 1e-16);
     bridgeCreator.Solve();
   }
 
