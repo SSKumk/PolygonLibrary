014b6ccfdd4f15c2a0b2f3f0342dc4625d34e9c5
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-01-27 16:06:24 +0500

    Update Main.cs

--- PolygonLibrary/Trajectories/Main.cs
+++ PolygonLibrary/Trajectories/Main.cs
@@ -10,12 +10,15 @@ public static class Trajectory {
     string ldgPath = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
     // string ldgPath  = "E:\\Work\\LDG\\";
 
-    double eps1 = 1e-08;
-    ddouble eps2 = 1e-16;
+    double epsD = 1e-08;
+    ddouble epsDD = 1e-16;
 
-    TrajectoryMain<double, DConvertor> traj = new TrajectoryMain<double, DConvertor>(ldgPath, "SimpleMotion.Test1", 1e-08);
-    traj.CalcTraj("1");
-    traj.CalcTraj("2");
+    // TrajectoryMain<double, DConvertor> traj = new TrajectoryMain<double, DConvertor>(ldgPath, "SimpleMotion.Test1", 1e-08);
+    TrajectoryMain<ddouble, DDConvertor> traj = new TrajectoryMain<ddouble, DDConvertor>(ldgPath, "Oscillator", epsDD);
+
+    // traj.CalcTraj("1");
+    // traj.CalcTraj("2");
+    traj.CalcTraj("3");
   }
 
 }
