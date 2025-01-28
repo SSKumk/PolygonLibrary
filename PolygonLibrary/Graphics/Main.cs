5d8c444ce0d2d5ffc74592deeaa1438b4fdbba75
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-01-28 17:10:48 +0500

    Update Main.cs

--- PolygonLibrary/Graphics/Main.cs
+++ PolygonLibrary/Graphics/Main.cs
@@ -10,8 +10,8 @@ public class Program {
     string pathLdg = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
     // string pathLdg = "E:\\Work\\LDG\\";
 
-    // Visualization vis = new Visualization(pathLdg, "Oscillator.Blender", "DoubleDouble.ddouble", 1e-015);
-    Visualization vis = new Visualization(pathLdg, "MassDot.Blender", "DoubleDouble.ddouble", 1e-015);
+    Visualization vis = new Visualization(pathLdg, "Oscillator.Blender", "DoubleDouble.ddouble", 1e-015);
+    // Visualization vis = new Visualization(pathLdg, "MassDot.Blender", "DoubleDouble.ddouble", 1e-015);
     vis.ForBlender();
 
 
