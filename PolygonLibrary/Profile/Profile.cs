eebd6ff83473e3071707aa18f01f3398b3c0850a
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-01-28 17:11:29 +0500

    Update Profile.cs

--- PolygonLibrary/Profile/Profile.cs
+++ PolygonLibrary/Profile/Profile.cs
@@ -1,11 +1,13 @@
 using System.Diagnostics;
 using System.Globalization;
 using Tests.ToolsTests;
+using LDG;
+using Tests;
+// using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
+using static CGLibrary.Geometry<double, Tests.DConvertor>;
 
-using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
-// using static CGLibrary.Geometry<double, Tests.DConvertor>;
-
-using static Tests.ToolsTests.TestsPolytopes<DoubleDouble.ddouble, Tests.DDConvertor>;
+// using static Tests.ToolsTests.TestsPolytopes<double, Tests.DConvertor>;
+// using static Tests.ToolsTests.TestsPolytopes<DoubleDouble.ddouble, Tests.DDConvertor>;
 
 
 namespace Profile;
@@ -17,43 +19,26 @@ class Program {
     "F:/Works/IMM/Аспирантура/_PolygonLibrary/CGLibrary/Tests/OtherTests/LDG_computations";
 
   static void Main(string[] args) {
-    // Tools.Eps = 1e-8;
-    Tools.Eps = 1e-16;
+    string pathLdg = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
 
-    Console.WriteLine($"{0.GetHashCode()}");
+    double epsD  = 1e-08;
+    double epsDD = 1e-15;
 
-     int dim = 5;
+    LDGPathHolder<double, DConvertor> ph =
+      new LDGPathHolder<double, DConvertor>(pathLdg, "Oscillator", "DoubleDouble.ddouble", epsDD);
 
-    // SolverLDG solverLdg = new SolverLDG(pathData, "MassDot");
-    // string    t         = "5.10";
-    // ParamReader prR = new ParamReader
-    // ($"{solverLdg.WorkDir}{solverLdg.gd.TaskDirToWriteInto}/{ftype}/Geometric/{eps}/{t}){solverLdg.FileName}.cpolytop");
-    // ConvexPolytop polytop = ConvexPolytop.CreateFromReader(prR);
-    // ConvexPolytop polytop = ConvexPolytop.Cube01_VRep(dim).GetInFLrep();
-    // var           _       = polytop.Hrep;
-    // ConvexPolytop polytop = MinkowskiSum.BySandipDas
-    // (ConvexPolytop.SimplexRND(dim), ConvexPolytop.SimplexRND(dim));
-    // HyperPlane hp = HyperPlane.Make3D_xyParallel(3);
-    // Vector v = Vector.MakeOrth(dim, 1);
-    // Console.WriteLine($"{hp.AffBasis.ProjectPointToSubSpace_in_OrigSpace(v)}");
-    // Console.WriteLine($"{x.Equals(res)}");
+    const double                            t      = 6.4;
+    SortedDictionary<double, ConvexPolytop> bridge = new SortedDictionary<double, ConvexPolytop>(Tools.TComp);
+    ph.LoadBridgeSection(bridge, 0, t);
+    var p = bridge[t];
+    // Console.WriteLine($"{p.NearestPoint(new Vector(new double[] { 0.2, 0.3, 3 }))}");
+    // Console.WriteLine($"{p.NearestPoint(new Vector(new double[] { 1.22, 0, 1 }))}"); // на грани  (1.0592591369594095,-0.0066899114567881834,1.290119495661504)
+    // Console.WriteLine($"{p.NearestPoint(new Vector(new double[] { -1.33, 0.1, 1 }))}"); //  на грани  (-1.1195813659540708,0.08769657244251747,1.3269287269958363)
 
-    // Console.WriteLine($"{Vector.Ones(6).ToStringBraceAndDelim(null, null, ' ')}");
+    // p.Vrep.TryGetValue()
 
-    // SolverLDG solverLdg = new SolverLDG(pathData, "SomeRND", false);
-    // SolverLDG solverLdg = new SolverLDG(pathData, "MassDot");
-    // // SolverLDG solverLdg = new SolverLDG(pathData, "oscillator");
-    // // SolverLDG solverLdg = new SolverLDG(pathData, "simpleMotion");
-    //
-    // // solverLdg.Solve(true);
-    // solverLdg.LoadGame(6,7);
-    // var x = solverLdg.Euler(new Vector(new ddouble[]{4,0,2}), 6, 7);
-    // Console.WriteLine($"{string.Join('\n', x)}");
-
-    // solverLdg.WorkOutControl(1*Vector.MakeOrth(3,1), 0, out Vector p, out Vector q);
-    // Console.WriteLine($"Управление P = {p}");
-    // Console.WriteLine($"Управление Q = {q}");
-
+   // на пересечении нескольких граней -0.2815753061702822063403420110214,1.290524349044212509360547779272,1.058374937912177279801186291598
+   // внутри -0.3 -0.5 1.3
   }
 
 }
