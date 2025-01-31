ec20cd90fec3b1b1db0f9b8d1f5bd7e86912070a
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-01-31 10:07:16 +0500

    Update Main.cs

--- PolygonLibrary/Trajectories/Main.cs
+++ PolygonLibrary/Trajectories/Main.cs
@@ -14,14 +14,18 @@ public static class Trajectory {
     double epsDD = 1e-15;
 
     // TrajectoryMain<double, DConvertor> traj = new TrajectoryMain<double, DConvertor>(ldgPath, "SimpleMotion.Test1", 1e-08);
-    TrajectoryMain<ddouble, DDConvertor> traj = new TrajectoryMain<ddouble, DDConvertor>(ldgPath, "Oscillator", epsDD);
     // TrajectoryMain<ddouble, DDConvertor> traj = new TrajectoryMain<ddouble, DDConvertor>(ldgPath, "MassDot", epsDD);
+    TrajectoryMain<ddouble, DDConvertor> traj = new TrajectoryMain<ddouble, DDConvertor>(ldgPath, "Oscillator", epsDD);
+    // TrajectoryMain<ddouble, DDConvertor> traj = new TrajectoryMain<ddouble, DDConvertor>(ldgPath, "Oscillator2D", epsDD);
 
     // traj.CalcTraj("1", true);
     // traj.CalcTraj("2", true);
     // traj.CalcTraj("3", true);
     // traj.CalcTraj("4", true);
-    traj.CalcTraj("5", true);
+    // traj.CalcTraj("5", true);
+
+    traj.CalcTraj("0.01");
+    traj.CalcTraj("0.001");
 
 
   }
