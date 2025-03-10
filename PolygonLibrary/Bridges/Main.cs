aa0aa54f7c18290d86ffc8205d21b005c37112f3
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-10 15:23:43 +0500

    Update Main.cs

--- PolygonLibrary/Bridges/Main.cs
+++ PolygonLibrary/Bridges/Main.cs
@@ -13,12 +13,20 @@ class Program {
     string ldgDir = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
     // string ldgDir = "E:\\Work\\LDG\\_Out\\";
 
+      ddouble eps  = ddouble.Parse("1e-8");
+    // ddouble eps = ddouble.Parse("1e-15");
+    // Rational eps = Rational.Parse("1/100000000000000000000000000000000");
 
+    // string problem = "Oscillator2D-square"; // double: (+)45'291, (-)43'672, (*)56'252, (/)6'189,
 
-    // BridgeCreator<double, DConvertor> bridgeCreator = new BridgeCreator<double, DConvertor>(ldgDir, "Oscillator", 1e-08);
-    // BridgeCreator<ddouble, DDConvertor> bridgeCreator = new BridgeCreator<ddouble, DDConvertor>(ldgDir, "MassDot", 1e-15);
-    // BridgeCreator<ddouble, DDConvertor> bridgeCreator = new BridgeCreator<ddouble, DDConvertor>(ldgDir, "Oscillator2D", 1e-15);
-    BridgeCreator<Rational, RConvertor> bridgeCreator = new BridgeCreator<Rational, RConvertor>(ldgDir, "Oscillator2D-square", (Rational)1e-30);
+    // string problem = "Oscillator3D"; // double: (+), (-), (*), (/),
+
+    string problem = "Oscillator"; // double: (+), (-), (*), (/),
+
+    BridgeCreator<double, DConvertor> bridgeCreator = new BridgeCreator<double, DConvertor>(ldgDir, problem, (double)eps);
+    // BridgeCreator<ddouble, DDConvertor> bridgeCreator = new BridgeCreator<ddouble, DDConvertor>(ldgDir, problem, eps);
+    // BridgeCreator<Rational, RConvertor> bridgeCreator = new BridgeCreator<Rational, RConvertor>(ldgDir, problem, (Rational)eps);
+
     bridgeCreator.Solve();
   }
 
