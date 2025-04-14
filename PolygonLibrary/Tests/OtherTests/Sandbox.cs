db4bae0cc345436cf543fda5798e209775370b13
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-04-14 14:59:25 +0500

    Update Sandbox.cs

--- PolygonLibrary/Tests/OtherTests/Sandbox.cs
+++ PolygonLibrary/Tests/OtherTests/Sandbox.cs
@@ -6,8 +6,8 @@ using static Tests.ToolsTests.TestsPolytopes<DoubleDouble.ddouble, Tests.DDConve
 using static Tests.ToolsTests.TestsBase<DoubleDouble.ddouble, Tests.DDConvertor>;
 using System.IO;
 using DoubleDouble;
-using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
-// using static CGLibrary.Geometry<double, Tests.DConvertor>;
+// using static CGLibrary.Geometry<DoubleDouble.ddouble, Tests.DDConvertor>;
+using static CGLibrary.Geometry<double, Tests.DConvertor>;
 
 
 namespace Tests.OtherTests;
@@ -20,20 +20,33 @@ public class Sandbox {
 
   [Test]
   public void Sandboxx() {
-    int dim = 5;
-    var x   = Cube01(dim, out List<Vector> cube);
-    var y   = SimplexRND(dim, out _);
-    ShiftAndRotate(dim, ref cube, ref x);
+    int       dim = 3;
+    GRandomLC rnd = new GRandomLC(1);
+    var       P   = ConvexPolytop.SimplexRND(dim, true, rnd);
+    var       Q   = ConvexPolytop.RectAxisParallel(Vector.Zero(dim), Vector.Ones(dim));
 
-    var xP = ConvexPolytop.CreateFromPoints(x);
-    var yP = ConvexPolytop.CreateFromPoints(y);
-    var xy = MinkowskiSum.BySandipDas(xP, yP);
-    
-    var h  = xy.Hrep;
+    ConvexPolytop polytop = MinkowskiSum.BySandipDas(P, Q);
 
-    var hG = ConvexPolytop.CreateFromPoints(ConvexPolytop.HrepToVrep_Geometric(h));
+    var res = ConvexPolytop.HrepToVrep_Geometric(polytop.Hrep);
+    Console.WriteLine($"polytop.Vrep == h2v: {polytop.Vrep.SetEquals(res)}");
+    Console.WriteLine($"polytope: {polytop.Vrep.Count}");
+    Console.WriteLine($"res: {res.Count}");
 
-    Console.WriteLine($"{xy.Vrep.SetEquals(hG.Vrep)}");
+    //
+    // int dim = 5;
+    // var x   = Cube01(dim, out List<Vector> cube);
+    // var y   = SimplexRND(dim, out _);
+    // ShiftAndRotate(dim, ref cube, ref x);
+    //
+    // var xP = ConvexPolytop.CreateFromPoints(x);
+    // var yP = ConvexPolytop.CreateFromPoints(y);
+    // var xy = MinkowskiSum.BySandipDas(xP, yP);
+    //
+    // var h  = xy.Hrep;
+    //
+    // var hG = ConvexPolytop.CreateFromPoints(ConvexPolytop.HrepToVrep_Geometric(h));
+    //
+    // Console.WriteLine($"{xy.Vrep.SetEquals(hG.Vrep)}");
   }
 
 
