18e9bf000ca48dc7686a6eab51556edae03c2916
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-04-14 13:54:46 +0500

    Update Main.cs

--- PolygonLibrary/Graphics/Main.cs
+++ PolygonLibrary/Graphics/Main.cs
@@ -1,4 +1,5 @@
-﻿using DoubleDouble;
+﻿using CGLibrary;
+using DoubleDouble;
 using Graphics.Draw;
 using LDG;
 using Rationals;
@@ -8,21 +9,34 @@ namespace Graphics;
 public class Program {
 
   public static void Main() {
-    string pathLdg = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
+    const string pathTemp = "F:/Temp/";
+
+    const string pathLdg = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
     // string pathLdg = "E:\\Work\\LDG\\";
 
-    Visualization<double, DConvertor>   visD  = new Visualization<double, DConvertor>(pathLdg, "Oscillator3D.Blender", 1e-08);
-    Visualization<ddouble, DDConvertor> visDD = new Visualization<ddouble, DDConvertor>(pathLdg, "Oscillator3D.Blender", ddouble.Parse("1e-08"));
-    Visualization<Rational, RConvertor> visR08 =
-      new Visualization<Rational, RConvertor>(pathLdg, "Oscillator3D.Blender", Rational.Parse("1/100000000"));
-    Visualization<Rational, RConvertor> visR16 =
-      new Visualization<Rational, RConvertor>(pathLdg, "Oscillator3D.Blender", Rational.Parse("1/10000000000000000"));
+    var polytope =
+      Geometry<double, DConvertor>.ConvexPolytop.CreateFromReader
+        (new Geometry<double, DConvertor>.ParamReader($"{pathTemp}polytope.polytope"));
+    var polytopeflrep =
+      Geometry<double, DConvertor>.ConvexPolytop.CreateFromReader
+        (new Geometry<double, DConvertor>.ParamReader($"{pathTemp}polytopeflrep.polytope"));
+
+    VisTools.DrawPolytopePLY(polytope, $"{pathTemp}polytope");
+    VisTools.DrawPolytopePLY(polytopeflrep, $"{pathTemp}polytopeflrep");
 
 
-    visD.ForBlender();
-    visDD.ForBlender();
-    visR08.ForBlender();
-    visR16.ForBlender();
+    // Visualization<double, DConvertor>   visD  = new Visualization<double, DConvertor>(pathLdg, "Oscillator3D.Blender", 1e-08);
+    // Visualization<ddouble, DDConvertor> visDD = new Visualization<ddouble, DDConvertor>(pathLdg, "Oscillator3D.Blender", ddouble.Parse("1e-08"));
+    // Visualization<Rational, RConvertor> visR08 =
+    //   new Visualization<Rational, RConvertor>(pathLdg, "Oscillator3D.Blender", Rational.Parse("1/100000000"));
+    // Visualization<Rational, RConvertor> visR16 =
+    //   new Visualization<Rational, RConvertor>(pathLdg, "Oscillator3D.Blender", Rational.Parse("1/10000000000000000"));
+    //
+    //
+    // visD.ForBlender();
+    // visDD.ForBlender();
+    // visR08.ForBlender();
+    // visR16.ForBlender();
 
 
     // string pathForTests = "F:\\Works\\IMM\\Проекты\\Визуализация для LDG\\Файлы многогранников\\";
