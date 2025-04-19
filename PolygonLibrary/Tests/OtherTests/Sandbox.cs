c5d01b0a673c05f7552355cbf66c14b9ba2605a3
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-04-19 11:47:50 +0500

    Update Sandbox.cs

--- PolygonLibrary/Tests/OtherTests/Sandbox.cs
+++ PolygonLibrary/Tests/OtherTests/Sandbox.cs
@@ -21,50 +21,53 @@ public class Sandbox {
 
   [Test]
   public void Sandboxx() {
-    int       dim              = 5;
-    // GRandomLC rnd        = new GRandomLC(0);
-
-    ConvexPolytop Q = ConvexPolytop.SimplexRND(dim);
-    // var      Q = ConvexPolytop.SimplexRND(2, false, rnd).LiftUp(4, 0);
-    // ConvexPolytop Q = ConvexPolytop.RectAxisParallel(Vector.Zero(dim), Vector.Ones(dim)).RotateRND().Shift(Vector.GenVector(dim, -10,10));
-
-    // var       P       = ConvexPolytop.SimplexRND(2, false, rnd).LiftUp(3, 0).RotateRND(true, rnd);
-    // ConvexPolytop P = ConvexPolytop.SimplexRND(dim);
-    ConvexPolytop P = ConvexPolytop.RectAxisParallel(Vector.Zero(dim), Vector.Ones(dim)).RotateRND();
-
-    ConvexPolytop polytop = MinkowskiSum.BySandipDas(P, Q);
-    // ConvexPolytop polytopCH = MinkowskiSum.ByConvexHull(P, Q);
+    Console.WriteLine($"{double.Acos(0.999999999)}");
 
 
-
-    // polytop.WriteIn(new ParamWriter("F:/Temp/polytope.polytope"), ConvexPolytop.Rep.FLrep);
-
-    var flrep         = HrepToFLrep.HrepToFLrep_Geometric(polytop.Hrep, dim);
-    var polytopeflrep = ConvexPolytop.CreateFromFaceLattice(flrep);
-    // polytopeflrep.WriteIn(new ParamWriter("F:/Temp/polytopeflrep.polytope"), ConvexPolytop.Rep.FLrep);
-
-
-    Console.WriteLine($"Polytope");
-    Console.WriteLine($"Number 3: {polytop.FLrep[^2].Count(node => node.Sub.Count == 3)}");
-    Console.WriteLine($"Number 4: {polytop.FLrep[^2].Count(node => node.Sub.Count == 4)}");
-    Console.WriteLine($"Number > 4: {polytop.FLrep[^2].Count(node => node.Sub.Count > 4)}");
-
+    // int       dim              = 5;
+    // // GRandomLC rnd        = new GRandomLC(0);
+    //
+    // ConvexPolytop Q = ConvexPolytop.SimplexRND(dim);
+    // // var      Q = ConvexPolytop.SimplexRND(2, false, rnd).LiftUp(4, 0);
+    // // ConvexPolytop Q = ConvexPolytop.RectAxisParallel(Vector.Zero(dim), Vector.Ones(dim)).RotateRND().Shift(Vector.GenVector(dim, -10,10));
+    //
+    // // var       P       = ConvexPolytop.SimplexRND(2, false, rnd).LiftUp(3, 0).RotateRND(true, rnd);
+    // // ConvexPolytop P = ConvexPolytop.SimplexRND(dim);
+    // ConvexPolytop P = ConvexPolytop.RectAxisParallel(Vector.Zero(dim), Vector.Ones(dim)).RotateRND();
+    //
+    // ConvexPolytop polytop = MinkowskiSum.BySandipDas(P, Q);
+    // // ConvexPolytop polytopCH = MinkowskiSum.ByConvexHull(P, Q);
+    //
+    //
+    //
+    // // polytop.WriteIn(new ParamWriter("F:/Temp/polytope.polytope"), ConvexPolytop.Rep.FLrep);
+    //
+    // var flrep         = HrepToFLrep.HrepToFLrep_Geometric(polytop.Hrep, dim);
+    // var polytopeflrep = ConvexPolytop.CreateFromFaceLattice(flrep);
+    // // polytopeflrep.WriteIn(new ParamWriter("F:/Temp/polytopeflrep.polytope"), ConvexPolytop.Rep.FLrep);
+    //
+    //
+    // Console.WriteLine($"Polytope");
+    // Console.WriteLine($"Number 3: {polytop.FLrep[^2].Count(node => node.Sub.Count == 3)}");
+    // Console.WriteLine($"Number 4: {polytop.FLrep[^2].Count(node => node.Sub.Count == 4)}");
+    // Console.WriteLine($"Number > 4: {polytop.FLrep[^2].Count(node => node.Sub.Count > 4)}");
+    //
+    // // Console.WriteLine($"");
+    // //
+    // // Console.WriteLine($"PolytopeCH");
+    // // Console.WriteLine($"Number 3: {polytopCH.FLrep[^2].Count(node => node.Sub.Count == 3)}");
+    // // Console.WriteLine($"Number 4: {polytopCH.FLrep[^2].Count(node => node.Sub.Count == 4)}");
+    // // Console.WriteLine($"Number > 4: {polytopCH.FLrep[^2].Count(node => node.Sub.Count > 4)}");
+    //
     // Console.WriteLine($"");
     //
-    // Console.WriteLine($"PolytopeCH");
-    // Console.WriteLine($"Number 3: {polytopCH.FLrep[^2].Count(node => node.Sub.Count == 3)}");
-    // Console.WriteLine($"Number 4: {polytopCH.FLrep[^2].Count(node => node.Sub.Count == 4)}");
-    // Console.WriteLine($"Number > 4: {polytopCH.FLrep[^2].Count(node => node.Sub.Count > 4)}");
-
-    Console.WriteLine($"");
-
-    Console.WriteLine($"V2FL");
-    Console.WriteLine($"Number 3: {polytopeflrep.FLrep[^2].Count(node => node.Sub.Count == 3)}");
-    Console.WriteLine($"Number 4: {polytopeflrep.FLrep[^2].Count(node => node.Sub.Count == 4)}");
-    Console.WriteLine($"Number > 4: {polytopeflrep.FLrep[^2].Count(node => node.Sub.Count > 4)}");
-
-    // Assert.That(polytop.FLrep.Equals(polytopCH));
-    Assert.That(polytop.FLrep.Equals(flrep));
+    // Console.WriteLine($"V2FL");
+    // Console.WriteLine($"Number 3: {polytopeflrep.FLrep[^2].Count(node => node.Sub.Count == 3)}");
+    // Console.WriteLine($"Number 4: {polytopeflrep.FLrep[^2].Count(node => node.Sub.Count == 4)}");
+    // Console.WriteLine($"Number > 4: {polytopeflrep.FLrep[^2].Count(node => node.Sub.Count > 4)}");
+    //
+    // // Assert.That(polytop.FLrep.Equals(polytopCH));
+    // Assert.That(polytop.FLrep.Equals(flrep));
 
   }
 
