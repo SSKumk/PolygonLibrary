9822c4bdf0c75c6d73634d7b16674a50ea6b3807
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-04-21 14:58:42 +0500

    +Sandbox proj

---
+++ PolygonLibrary/Sandbox/Sandbox.cs
@@ -0,0 +1,19 @@
+﻿using static CGLibrary.Geometry<double, Sandbox.DConvertor>;
+
+
+namespace Sandbox;
+
+class Sandbox {
+
+  static void Main(string[] args) {
+    GRandomLC   rnd   = new GRandomLC(4567);
+    LinearBasis basis = LinearBasis.GenLinearBasis(5, 3, rnd);
+
+    Vector v        = Vector.GenVector(5, rnd);
+
+    Vector prv = basis.ProjectVectorToSubSpace_in_OrigSpace(v);
+
+    Console.WriteLine($"{prv}");
+  }
+
+}
