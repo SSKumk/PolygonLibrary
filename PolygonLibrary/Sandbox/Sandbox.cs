7fda2c379150e36dc1065f55a1867400a93461a1
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-04-22 19:29:31 +0500

    Update Sandbox.cs

--- PolygonLibrary/Sandbox/Sandbox.cs
+++ PolygonLibrary/Sandbox/Sandbox.cs
@@ -6,21 +6,23 @@ namespace Sandbox;
 class Sandbox {
 
   static void Main(string[] args) {
+    GRandomLC   rnd   = new GRandomLC(1);
 
-    GRandomLC rnd = new GRandomLC(1);
-    Matrix    m   = Matrix.GenMatrixInt(3,4, -10,10);
-
-    Console.WriteLine($"{m}");
-
-    Vector v = Vector.GenVectorInt(4,6,7,rnd);
-
-    Console.WriteLine($"{v}");
-
-    Console.WriteLine($"{Matrix.vcat(m,v)}");
-
-
-
+    Matrix m = new Matrix(new double[,]{ { 2.0, 3, 4 }});
+    Console.WriteLine($"{m.ToRREF()}");
+    // LinearBasis basis = LinearBasis.GenLinearBasis(3, 2);
+    //
+    // Vector n = basis.FindOrthonormalVector();
+    //
+    // HyperPlane hp = new HyperPlane(n, Vector.Zero(3));
+    //
+    //
+    // Console.WriteLine($"{basis}");
+    // LinearBasis linBasis = hp.AffBasis.LinBasis;
+    // Console.WriteLine($"{linBasis}");
+    // Console.WriteLine($"same space? {linBasis.SpanSameSpace(basis)}\n");
+    // Console.WriteLine($"{basis.Basis.ToRREF()}\n");
+    // Console.WriteLine($"{linBasis.Basis.ToRREF()}");
   }
-
 
 }
