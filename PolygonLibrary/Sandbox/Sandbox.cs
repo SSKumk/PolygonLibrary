9c8ea2c85cb35d7be2a94601c0f0c21e8cf5e952
Anton Mikhailov <tigr-mav@yandex.ru>
2025-04-28 16:00:23 +0500

    Update Sandbox.cs

--- PolygonLibrary/Sandbox/Sandbox.cs
+++ PolygonLibrary/Sandbox/Sandbox.cs
@@ -1,4 +1,5 @@
-﻿using static CGLibrary.Geometry<double, Sandbox.DConvertor>;
+﻿using System.Diagnostics;
+using static CGLibrary.Geometry<double, Sandbox.DConvertor>;
 
 
 namespace Sandbox;
@@ -8,8 +9,42 @@ class Sandbox {
   static void Main(string[] args) {
     GRandomLC   rnd   = new GRandomLC(1);
 
-    Matrix m = new Matrix(new double[,]{ { 2.0, 3, 4 }});
-    Console.WriteLine($"{m.ToRREF()}");
+    int dim = 5;
+
+    
+    ConvexPolytop polytop1 = ConvexPolytop.SimplexRND(dim);
+    //ConvexPolytop polytop2 = ConvexPolytop.SimplexRND(dim);
+    
+    ConvexPolytop polytop2 = ConvexPolytop.Cube01_VRep(dim);
+
+    ConvexPolytop res = MinkowskiSum.BySandipDas(polytop1, polytop2);
+
+    _     = res.Hrep;
+    Stopwatch timer = new Stopwatch();
+    
+    timer.Restart();
+    // previous approach
+    var x = ConvexPolytop.HrepToVrep_Geometric(res.Hrep);
+    var y = ConvexPolytop.CreateFromPoints(x!);
+    _ = y.FLrep;
+    timer.Stop();
+    Console.WriteLine($"old: {timer.Elapsed.Seconds}");
+    Console.WriteLine($"old f-vec: {string.Join(", ",y.fVector)}");
+    
+    
+    timer.Restart();
+    // new approach
+    ConvexPolytop nwe = ConvexPolytop.CreateFromFaceLattice(HrepToFLrep.HrepToFLrep_Geometric(res.Hrep, 5)!);
+
+    timer.Stop();
+    Console.WriteLine($"new: {timer.Elapsed.Seconds}");
+    Console.WriteLine($"new f-vec: {string.Join(", ",nwe.fVector)}");
+    
+    Console.WriteLine($"res = {string.Join(", ",res.fVector)}");
+    Console.WriteLine($"{y.FLrep.Equals(nwe.FLrep)}");
+
+    // Matrix m = new Matrix(new double[,]{ { 2.0, 3, 4 }});
+    // Console.WriteLine($"{m.ToRREF()}");
     // LinearBasis basis = LinearBasis.GenLinearBasis(3, 2);
     //
     // Vector n = basis.FindOrthonormalVector();
