4876ed34abf18522b47dda0a256a591a63912aa0
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-05-12 14:16:42 +0500

    Update Sandbox.cs

--- PolygonLibrary/Sandbox/Sandbox.cs
+++ PolygonLibrary/Sandbox/Sandbox.cs
@@ -5,24 +5,64 @@ namespace Sandbox;
 
 class Sandbox {
 
-  static void Main(string[] args) {
-    GRandomLC   rnd   = new GRandomLC(1);
+  static bool Orthg(Matrix A) {
+    bool   res = true;
+    Matrix eye = Matrix.Eye(A.Rows);
+    res &= (A.Transpose() * A).Equals(eye);
+    res &= (A * A.Transpose()).Equals(eye);
 
-    Matrix m = new Matrix(new double[,]{ { 2.0, 3, 4 }});
-    Console.WriteLine($"{m.ToRREF()}");
-    // LinearBasis basis = LinearBasis.GenLinearBasis(3, 2);
-    //
-    // Vector n = basis.FindOrthonormalVector();
-    //
-    // HyperPlane hp = new HyperPlane(n, Vector.Zero(3));
-    //
-    //
-    // Console.WriteLine($"{basis}");
-    // LinearBasis linBasis = hp.AffBasis.LinBasis;
-    // Console.WriteLine($"{linBasis}");
-    // Console.WriteLine($"same space? {linBasis.SpanSameSpace(basis)}\n");
-    // Console.WriteLine($"{basis.Basis.ToRREF()}\n");
-    // Console.WriteLine($"{linBasis.Basis.ToRREF()}");
+    return res;
+  }
+
+
+  static void Main(string[] args) {
+    var rnd = new GRandomLC(1);
+    int dim = 5;
+
+    // Генерируем независимые векторы
+    var v1 = Vector.GenVector(dim, rnd);
+    var v2 = Vector.GenVector(dim, rnd);
+    var v3 = Vector.GenVector(dim, rnd);
+
+    // var v1 = 2*Vector.MakeOrth(dim, 1);
+    // var v2 = 3*Vector.MakeOrth(dim, 2);
+    // var v3 = 0.5*Vector.MakeOrth(dim, 3);
+
+    // Зависимый (вставлен между v2 и v3)
+    var dep = v1 + v2;
+
+    // Составляем список в нужном порядке:
+    var vectors =
+      new List<(Vector vec, string name)>
+        {
+          (v1, "v1")
+        , (v2, "v2")
+        , (dep, "dep")
+        , (v2, "v2 снова")
+        , (v3, "v3")
+        };
+
+    var lb = LinearBasis.GenLinearBasis(dim, 0, rnd);
+    int nk = 0;
+    int ns = 0;
+    var q  = new MutableMatrix(Matrix.Eye(dim));
+    var w  = new MutableMatrix(Matrix.Eye(dim));
+
+    // Локальный метод для обработки одного вектора
+    void Process((Vector vec, string name) item) {
+      nk = Decomposition.LQ_FullUpdate(ref q, nk, item.vec);
+      ns = Decomposition.QR_FullUpdate(ref w, ns, item.vec);
+
+      bool added = lb.AddVector(item.vec);
+      Console.WriteLine($"{item.name}: nk={nk}, ns={ns}, added={added}");
+      Console.WriteLine(lb);
+      Console.WriteLine(q);
+      Console.WriteLine(w.Transpose());
+    }
+
+    // Проходим по списку ровно в том порядке, в котором он задан
+    foreach (var item in vectors)
+      Process(item);
   }
 
 }
