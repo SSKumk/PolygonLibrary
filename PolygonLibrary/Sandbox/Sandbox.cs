6f3080cc630c5e199444ac908621426bbd58af5d
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-05-13 17:01:02 +0500

    Update Sandbox.cs

--- PolygonLibrary/Sandbox/Sandbox.cs
+++ PolygonLibrary/Sandbox/Sandbox.cs
@@ -14,55 +14,20 @@ class Sandbox {
     return res;
   }
 
+  public static Vector V(params double[] coords) => new Vector(coords);
 
   static void Main(string[] args) {
-    var rnd = new GRandomLC(1);
-    int dim = 5;
 
-    // Генерируем независимые векторы
-    var v1 = Vector.GenVector(dim, rnd);
-    var v2 = Vector.GenVector(dim, rnd);
-    var v3 = Vector.GenVector(dim, rnd);
+    int                dim = 100;
+    LinearBasisMutable lb  = new LinearBasisMutable(dim,30);
+    // AffineBasis        ab  = new AffineBasis(Vector.Zero(dim), lb, needCopy: true);
+    // AffineBasis        ab  = new AffineBasis(Vector.Zero(dim), lb, needCopy: false);
 
-    // var v1 = 2*Vector.MakeOrth(dim, 1);
-    // var v2 = 3*Vector.MakeOrth(dim, 2);
-    // var v3 = 0.5*Vector.MakeOrth(dim, 3);
+    // Console.WriteLine($"{ab.Empty}");
 
-    // Зависимый (вставлен между v2 и v3)
-    var dep = v1 + v2;
 
-    // Составляем список в нужном порядке:
-    var vectors =
-      new List<(Vector vec, string name)>
-        {
-          (v1, "v1")
-        , (v2, "v2")
-        , (dep, "dep")
-        , (v2, "v2 снова")
-        , (v3, "v3")
-        };
 
-    LinearBasisMutable lb = LinearBasisMutable.GenLinearBasis(dim, 0, rnd);
-    int nk = 0;
-    int ns = 0;
-    var q  = new MatrixMutable(Matrix.Eye(dim), false);
-    var w  = new MatrixMutable(Matrix.Eye(dim), false);
 
-    // Локальный метод для обработки одного вектора
-    void Process((Vector vec, string name) item) {
-      nk = Decomposition.LQ_FullUpdate(ref q, nk, item.vec);
-      ns = Decomposition.QR_FullUpdate(ref w, ns, item.vec);
-
-      bool added = lb.AddVector(item.vec);
-      Console.WriteLine($"{item.name}: nk={nk}, ns={ns}, added={added}");
-      Console.WriteLine(lb);
-      Console.WriteLine(q);
-      Console.WriteLine(w.Transpose());
-    }
-
-    // Проходим по списку ровно в том порядке, в котором он задан
-    foreach (var item in vectors)
-      Process(item);
   }
 
 }
