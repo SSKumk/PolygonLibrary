d9cb00ead5a992707bda3d9725b5ed5cf117c5aa
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-05-27 22:51:33 +0500

    Update Main.cs

--- PolygonLibrary/Graphics/Main.cs
+++ PolygonLibrary/Graphics/Main.cs
@@ -14,21 +14,31 @@ public class Program {
     const string pathLdg = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
     // string pathLdg = "E:\\Work\\LDG\\";
 
-    Visualization<double, DConvertor>   visD  = new Visualization<double, DConvertor>(pathLdg, "Oscillator-mass.Blender", 1e-08);
-    // Visualization<ddouble, DDConvertor> visDD = new Visualization<ddouble, DDConvertor>(pathLdg, "Oscillator3D.Blender", ddouble.Parse("1e-08"));
+    string visConfig = "Oscillator-pyramid.Blender";
+    // string visConfig = "Oscillator.Blender";
+    // string visConfig = "Oscillator-mass.Blender";
+    // string visConfig = "MassDot-mass.Blender";
+    
+    // Visualization<double, DConvertor>   visD  = new Visualization<double, DConvertor>(pathLdg, visConfig, 1e-08);
+    // Visualization<ddouble, DDConvertor> visDD = new Visualization<ddouble, DDConvertor>(pathLdg, visConfig, ddouble.Parse("1e-08"));
     // Visualization<Rational, RConvertor> visR08 =
-      // new Visualization<Rational, RConvertor>(pathLdg, "Oscillator3D.Blender", Rational.Parse("1/100000000"));
+      // new Visualization<Rational, RConvertor>(pathLdg, visConfig, Rational.Parse("1/100000000"));
     // Visualization<Rational, RConvertor> visR16 =
-      // new Visualization<Rational, RConvertor>(pathLdg, "Oscillator3D.Blender", Rational.Parse("1/10000000000000000"));
+      // new Visualization<Rational, RConvertor>(pathLdg, visConfig, Rational.Parse("1/10000000000000000"));
 
 
-    visD.ForBlender();
+    // visD.ForBlender();
     // visDD.ForBlender();
     // visR08.ForBlender();
     // visR16.ForBlender();
 
 
-    // string pathForTests = "F:\\Works\\IMM\\Проекты\\Визуализация для LDG\\Файлы многогранников\\";
+
+    string pathForTests = "F:\\Works\\IMM\\Аспирантура\\LDG\\Visualization\\Temp\\";
+    Geometry<double,DConvertor>.ConvexPolytop sumCH = Geometry<double, DConvertor>.ConvexPolytop.CreateFromReader(new Geometry<double, DConvertor>.ParamReader($"{pathForTests}pyramid-CH.cpolytope"));
+    Geometry<double,DConvertor>.ConvexPolytop sum = Geometry<double, DConvertor>.ConvexPolytop.CreateFromReader(new Geometry<double, DConvertor>.ParamReader($"{pathForTests}pyramid.cpolytope"));
+    VisTools.DrawPolytopePLY(sumCH, $"{pathForTests}pyramid-CH");
+    VisTools.DrawPolytopePLY(sum, $"{pathForTests}pyramid");
   }
 
 }
