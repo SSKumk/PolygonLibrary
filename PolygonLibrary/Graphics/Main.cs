e5c0c683a480c1777d0c6f4929bb1c0c8b41041f
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-05-30 13:57:31 +0500

    Update Main.cs

--- PolygonLibrary/Graphics/Main.cs
+++ PolygonLibrary/Graphics/Main.cs
@@ -18,9 +18,13 @@ public class Program {
     // string visConfig = "Oscillator.Blender";
     // string visConfig = "Oscillator-mass.Blender";
     // string visConfig = "MassDot-mass.Blender";
-    
-    // Visualization<double, DConvertor>   visD  = new Visualization<double, DConvertor>(pathLdg, visConfig, 1e-08);
-    // Visualization<ddouble, DDConvertor> visDD = new Visualization<ddouble, DDConvertor>(pathLdg, visConfig, ddouble.Parse("1e-08"));
+
+    double eps  = double.Parse("1e-8");
+    // ddouble eps = ddouble.Parse("1e-15");
+    // Rational eps = Rational.Parse("1/10000000000000000");
+
+    Visualization<double, DConvertor>   visD  = new Visualization<double, DConvertor>(pathLdg, visConfig, eps);
+    // Visualization<ddouble, DDConvertor> visDD = new Visualization<ddouble, DDConvertor>(pathLdg, visConfig, eps);
     // Visualization<Rational, RConvertor> visR08 =
       // new Visualization<Rational, RConvertor>(pathLdg, visConfig, Rational.Parse("1/100000000"));
     // Visualization<Rational, RConvertor> visR16 =
@@ -28,17 +32,17 @@ public class Program {
 
 
     // visD.ForBlender();
-    // visDD.ForBlender();
+    visDD.ForBlender();
     // visR08.ForBlender();
     // visR16.ForBlender();
 
 
 
-    string pathForTests = "F:\\Works\\IMM\\Аспирантура\\LDG\\Visualization\\Temp\\";
-    Geometry<double,DConvertor>.ConvexPolytop sumCH = Geometry<double, DConvertor>.ConvexPolytop.CreateFromReader(new Geometry<double, DConvertor>.ParamReader($"{pathForTests}pyramid-CH.cpolytope"));
-    Geometry<double,DConvertor>.ConvexPolytop sum = Geometry<double, DConvertor>.ConvexPolytop.CreateFromReader(new Geometry<double, DConvertor>.ParamReader($"{pathForTests}pyramid.cpolytope"));
-    VisTools.DrawPolytopePLY(sumCH, $"{pathForTests}pyramid-CH");
-    VisTools.DrawPolytopePLY(sum, $"{pathForTests}pyramid");
+    // string pathForTests = "F:\\Works\\IMM\\Аспирантура\\LDG\\Visualization\\Temp\\";
+    // Geometry<double,DConvertor>.ConvexPolytop sumCH = Geometry<double, DConvertor>.ConvexPolytop.CreateFromReader(new Geometry<double, DConvertor>.ParamReader($"{pathForTests}pyramid-CH.cpolytope"));
+    // Geometry<double,DConvertor>.ConvexPolytop sum = Geometry<double, DConvertor>.ConvexPolytop.CreateFromReader(new Geometry<double, DConvertor>.ParamReader($"{pathForTests}pyramid.cpolytope"));
+    // VisTools.DrawPolytopePLY(sumCH, $"{pathForTests}pyramid-CH");
+    // VisTools.DrawPolytopePLY(sum, $"{pathForTests}pyramid");
   }
 
 }
