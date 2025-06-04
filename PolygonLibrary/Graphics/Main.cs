8615ce996a50ab87d115eeb575d89d91ceb3dc77
Anton Mikhailov <tigr-mav@yandex.ru>
2025-06-04 16:31:25 +0500

    Update Main.cs

--- PolygonLibrary/Graphics/Main.cs
+++ PolygonLibrary/Graphics/Main.cs
@@ -9,17 +9,18 @@ namespace Graphics;
 public class Program {
 
   public static void Main() {
-    const string pathTemp = "F:/Temp/";
+    // const string pathTemp = "F:/Temp/";
 
-    const string pathLdg = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
-    // string pathLdg = "E:\\Work\\LDG\\";
+    // const string pathLdg = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
+    string pathLdg = "E:\\Work\\LDG\\";
 
-    string visConfig = "Oscillator-pyramid.Blender";
+    // string visConfig = "Oscillator-hexagon.Blender";
+    // string visConfig = "Oscillator-pyramid.Blender";
     // string visConfig = "Oscillator.Blender";
-    // string visConfig = "Oscillator-mass.Blender";
+    string visConfig = "Oscillator-mass.Blender";
     // string visConfig = "MassDot-mass.Blender";
 
-    double eps  = double.Parse("1e-3");
+    double eps  = double.Parse("1e-8");
     // ddouble eps = ddouble.Parse("1e-15");
     // Rational eps = Rational.Parse("1/10000000000000000");
 
@@ -35,14 +36,6 @@ public class Program {
     // visDD.ForBlender();
     // visR08.ForBlender();
     // visR16.ForBlender();
-
-
-
-    // string pathForTests = "F:\\Works\\IMM\\Аспирантура\\LDG\\Visualization\\Temp\\";
-    // Geometry<double,DConvertor>.ConvexPolytop sumCH = Geometry<double, DConvertor>.ConvexPolytop.CreateFromReader(new Geometry<double, DConvertor>.ParamReader($"{pathForTests}pyramid-CH.cpolytope"));
-    // Geometry<double,DConvertor>.ConvexPolytop sum = Geometry<double, DConvertor>.ConvexPolytop.CreateFromReader(new Geometry<double, DConvertor>.ParamReader($"{pathForTests}pyramid.cpolytope"));
-    // VisTools.DrawPolytopePLY(sumCH, $"{pathForTests}pyramid-CH");
-    // VisTools.DrawPolytopePLY(sum, $"{pathForTests}pyramid");
   }
 
 }
