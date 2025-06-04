4c854cad69ad09b6dc2489d93b7c81bb79523a43
Anton Mikhailov <tigr-mav@yandex.ru>
2025-06-04 16:31:20 +0500

    Update Main.cs

--- PolygonLibrary/Bridges/Main.cs
+++ PolygonLibrary/Bridges/Main.cs
@@ -10,10 +10,10 @@ namespace Bridges;
 class Program {
 
   static void Main(string[] args) {
-    string ldgDir = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
-    // string ldgDir = "E:\\Work\\LDG\\_Out\\";
+    // string ldgDir = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
+    string ldgDir = "E:\\Work\\LDG\\";
 
-    double eps = double.Parse("1e-4");
+    double eps = double.Parse("1e-8");
     // ddouble eps = ddouble.Parse("1e-15");
     // Rational eps = Rational.Parse("1/10000000000000000");
 
@@ -23,8 +23,10 @@ class Program {
     // string problem = "Oscillator"; // double: (+), (-), (*), (/),
 
 
-    string problem = "Oscillator-pyramid";
-    // string problem = "Oscillator-mass";
+    // string problem = "Oscillator-hexagon";
+    // string problem = "Oscillator-pyramid";
+    // string problem = "Oscillator";
+    string problem = "Oscillator-mass";
     // string problem = "MassDot-mass";
 
     BridgeCreator<double, DConvertor> bridgeCreator = new BridgeCreator<double, DConvertor>(ldgDir, problem, eps);
