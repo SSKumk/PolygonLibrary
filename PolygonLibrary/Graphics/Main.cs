7caa5df4509440e11a9d635f43e6512477bbba23
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-06-05 18:41:18 +0500

    Update Main.cs

--- PolygonLibrary/Graphics/Main.cs
+++ PolygonLibrary/Graphics/Main.cs
@@ -11,21 +11,19 @@ public class Program {
   public static void Main() {
     // const string pathTemp = "F:/Temp/";
 
-    // const string pathLdg = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
-    string pathLdg = "E:\\Work\\LDG\\";
+    const string pathLdg = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
+    // string pathLdg = "E:\\Work\\LDG\\";
 
-    // string visConfig = "Oscillator-hexagon.Blender";
-    // string visConfig = "Oscillator-pyramid.Blender";
-    // string visConfig = "Oscillator.Blender";
-    string visConfig = "Oscillator-mass.Blender";
-    // string visConfig = "MassDot-mass.Blender";
+    string visConfig = "Oscillator-cone6.Blender";
+    // string visConfig = "Oscillator-mass-cone6.Blender";
+    // string visConfig = "Oscillator3D-mass-cone6.Blender";
 
-    double eps  = double.Parse("1e-8");
-    // ddouble eps = ddouble.Parse("1e-15");
+    // double eps  = double.Parse("1e-8");
+    ddouble eps = ddouble.Parse("1e-15");
     // Rational eps = Rational.Parse("1/10000000000000000");
 
-    Visualization<double, DConvertor>   visD  = new Visualization<double, DConvertor>(pathLdg, visConfig, eps);
-    // Visualization<ddouble, DDConvertor> visDD = new Visualization<ddouble, DDConvertor>(pathLdg, visConfig, eps);
+    // Visualization<double, DConvertor>   visD  = new Visualization<double, DConvertor>(pathLdg, visConfig, eps);
+    Visualization<ddouble, DDConvertor> visD = new Visualization<ddouble, DDConvertor>(pathLdg, visConfig, eps);
     // Visualization<Rational, RConvertor> visR08 =
       // new Visualization<Rational, RConvertor>(pathLdg, visConfig, Rational.Parse("1/100000000"));
     // Visualization<Rational, RConvertor> visR16 =
@@ -33,9 +31,6 @@ public class Program {
 
 
     visD.ForBlender();
-    // visDD.ForBlender();
-    // visR08.ForBlender();
-    // visR16.ForBlender();
   }
 
 }
