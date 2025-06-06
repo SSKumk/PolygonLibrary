47e9a32aba314168a80a9118c22d6988415ead15
Anton Mikhailov <tigr-mav@yandex.ru>
2025-06-06 16:38:20 +0500

    Update Sandbox.cs

--- PolygonLibrary/Sandbox/Sandbox.cs
+++ PolygonLibrary/Sandbox/Sandbox.cs
@@ -9,14 +9,22 @@ class Sandbox {
   // public static Vector V(params ddouble[] coords) => new Vector(coords);
 
   static void Main(string[] args) {
-    const string ppath = @"F:\Works\IMM\Аспирантура\LDG\Polytopes\";
+    // const string ppath = @"F:\Works\IMM\Аспирантура\LDG\Polytopes\";
+    const string ppath = @"E:\Work\LDG\Polytopes\";
 
-    string      pname = "6";
-    ConvexPolytop p = ConvexPolytop.DistanceToPointBall_2(Vector.Zero(2), 6, -1, 1);
+    ConvexPolytop hex2d   = ConvexPolytop.Sphere(Vector.Zero(2), 1, 6,-1);
+    var           hex3d_0 = hex2d.LiftUp(3, -1);
+    var           hex3d_1 = hex2d.LiftUp(3, 1);
+    hex3d_1.Vrep.UnionWith(hex3d_0.Vrep);
+    var p = ConvexPolytop.CreateFromPoints(hex3d_1.Vrep,true);
+    
+    
+    
+    string      pname = "7";
     p.WriteIn(ppath, pname, ConvexPolytop.Rep.FLrep);
+    
 
-
-
+    // Console.WriteLine();
 
   }
 
