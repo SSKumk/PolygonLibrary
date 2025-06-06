f2806361935b429cc1ab15b7b7d80af8727810e0
Anton Mikhailov <tigr-mav@yandex.ru>
2025-06-06 16:37:20 +0500

    Update Main.cs

--- PolygonLibrary/Graphics/Main.cs
+++ PolygonLibrary/Graphics/Main.cs
@@ -11,8 +11,8 @@ public class Program {
   public static void Main() {
     // const string pathTemp = "F:/Temp/";
 
-    const string pathLdg = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
-    // string pathLdg = "E:\\Work\\LDG\\";
+    // const string pathLdg = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
+    string pathLdg = "E:\\Work\\LDG\\";
 
     // string visConfig = "Oscillator-cone6.Blender";
     // string visConfig = "Oscillator-mass-cone6.Blender";
@@ -23,14 +23,17 @@ public class Program {
     // Rational eps = Rational.Parse("1/10000000000000000");
 
     // Visualization<double, DConvertor>   visD  = new Visualization<double, DConvertor>(pathLdg, visConfig, eps);
-    Visualization<ddouble, DDConvertor> visD = new Visualization<ddouble, DDConvertor>(pathLdg, visConfig, eps);
+    // Visualization<ddouble, DDConvertor> visD = new Visualization<ddouble, DDConvertor>(pathLdg, visConfig, eps);
     // Visualization<Rational, RConvertor> visR08 =
       // new Visualization<Rational, RConvertor>(pathLdg, visConfig, Rational.Parse("1/100000000"));
     // Visualization<Rational, RConvertor> visR16 =
       // new Visualization<Rational, RConvertor>(pathLdg, visConfig, Rational.Parse("1/10000000000000000"));
 
+      // visD.ForBlender();
 
-    visD.ForBlender();
+      string polytopePath = Path.Combine(pathLdg, "Polytopes");
+      string polytopeName = "7";
+      Visualization<ddouble, DDConvertor>.ReadAndDrawPolytopePLY(polytopePath, polytopeName);
   }
 
 }
