39ce5c71e0c61634767a3e239e5f2a8b2c6d8eee
Anton Mikhailov <tigr-mav@yandex.ru>
2025-06-06 16:35:08 +0500

    Update Main.cs

--- PolygonLibrary/Bridges/Main.cs
+++ PolygonLibrary/Bridges/Main.cs
@@ -10,20 +10,21 @@ namespace Bridges;
 class Program {
 
   static void Main(string[] args) {
-    string ldgDir = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
-    // string ldgDir = "E:\\Work\\LDG\\";
+    // string ldgDir = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
+    string ldgDir = "E:\\Work\\LDG\\";
 
-    // double eps = double.Parse("1e-8");
-    ddouble eps = ddouble.Parse("1e-15");
+    double eps = double.Parse("1e-8");
+    // ddouble eps = ddouble.Parse("1e-15");
     // Rational eps = Rational.Parse("1/10000000000000000");
 
 
-    string problem = "Oscillator-cone6";
+    // string problem = "Oscillator-cone6";
     // string problem = "Oscillator-mass-cone6";
-    // string problem = "Oscillator3D-mass-cone6";
+    string problem = "Oscillator3D-mass-cone6";
+    // string problem = "Oscillator3D-mass-epi-hex_prism";
 
-    // BridgeCreator<double, DConvertor> bridgeCreator = new BridgeCreator<double, DConvertor>(ldgDir, problem, eps);
-    BridgeCreator<ddouble, DDConvertor> bridgeCreator = new BridgeCreator<ddouble, DDConvertor>(ldgDir, problem, eps);
+    BridgeCreator<double, DConvertor> bridgeCreator = new BridgeCreator<double, DConvertor>(ldgDir, problem, eps);
+    // BridgeCreator<ddouble, DDConvertor> bridgeCreator = new BridgeCreator<ddouble, DDConvertor>(ldgDir, problem, eps);
     // BridgeCreator<Rational, RConvertor> bridgeCreator = new BridgeCreator<Rational, RConvertor>(ldgDir, problem, (Rational)eps);
 
     bridgeCreator.Solve();
