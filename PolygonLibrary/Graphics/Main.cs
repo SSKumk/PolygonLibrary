ec9456c7a773ba59a51cfd0c86f8d4091639cecb
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-07-09 18:57:07 +0500

    Update Main.cs

--- PolygonLibrary/Graphics/Main.cs
+++ PolygonLibrary/Graphics/Main.cs
@@ -14,7 +14,8 @@ public class Program {
     const string pathLdg = "F:\\Works\\IMM\\Аспирантура\\LDG\\";
     // string pathLdg = "E:\\Work\\LDG\\";
 
-    string visConfig = "Oscillator-triangle.Blender";
+    // string visConfig = "Oscillator-triangle.Blender";
+    string visConfig = "Oscillator-triangle_x100.Blender";
 
 
     // string visConfig = "Oscillator-cone6.Blender";
@@ -22,8 +23,8 @@ public class Program {
     // string visConfig = "Oscillator-mass-cone6.Blender";
     // string visConfig = "Oscillator3D-mass-cone6.Blender";
 
-    // double eps  = double.Parse("1e-3");
-    double eps  = double.Parse("1e-8");
+    double eps  = double.Parse("1e-3");
+    // double eps  = double.Parse("1e-8");
     // ddouble eps = ddouble.Parse("1e-15");
     // Rational eps = Rational.Parse("1/10000000000000000");
 
